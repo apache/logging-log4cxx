5ccd373ed7fccfcd83e9820b9be914ce7a2f936f
Michael Catanzariti <mcatan@apache.org>
2003-06-12 00:22:28 +0000

    Can now send events in XML format over TCP. log4j Chainsaw UI compatibility.

---
+++ include/log4cxx/net/xmlsocketappender.h
@@ -0,0 +1,280 @@
+/***************************************************************************
+                          xmlsocketappender.h  -  class XMLSocketAppender
+                             -------------------
+    begin                : jeu mai 8 2003
+    copyright            : (C) 2003 by Michael CATANZARITI
+    email                : mcatan@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.      *
+ *                                                                         *
+ * This software is published under the terms of the Apache Software       *
+ * License version 1.1, a copy of which has been included with this        *
+ * distribution in the LICENSE.txt file.                                   *
+ ***************************************************************************/
+
+#ifndef _LOG4CXX_NET_XML_SOCKET_APPENDER_H
+#define _LOG4CXX_NET_XML_SOCKET_APPENDER_H
+ 
+#include <log4cxx/appenderskeleton.h>
+#include <log4cxx/helpers/socket.h>
+#include <log4cxx/helpers/thread.h>
+
+namespace log4cxx
+{
+	namespace helpers
+	{
+		class SocketOutputStream;
+		typedef helpers::ObjectPtr<SocketOutputStream> SocketOutputStreamPtr;
+	};
+
+	namespace net
+	{
+		class XMLSocketAppender;
+		typedef helpers::ObjectPtr<XMLSocketAppender> XMLSocketAppenderPtr;
+
+        /**
+        Sends {@link spi::LoggingEvent LoggingEvent} objects to a remote a log server,
+        usually a {@link net::SocketNode SocketNode}.
+
+        <p>The SocketAppender has the following properties:
+
+        <ul>
+
+        <p><li>If sent to a {@link net::SocketNode SocketNode}, remote logging
+		is non-intrusive as far as the log event is concerned. In other
+        words, the event will be logged with the same time stamp, {@link
+        NDC NDC}, location info as if it were logged locally by
+        the client.
+
+        <p><li>SocketAppenders do not use a layout. They ship a
+        serialized {@link spi::LoggingEvent LoggingEvent} object
+		to the server side.
+
+        <p><li>Remote logging uses the TCP protocol. Consequently, if
+        the server is reachable, then log events will eventually arrive
+        at the server.
+
+        <p><li>If the remote server is down, the logging requests are
+        simply dropped. However, if and when the server comes back up,
+        then event transmission is resumed transparently. This
+        transparent reconneciton is performed by a <em>connector</em>
+        thread which periodically attempts to connect to the server.
+
+        <p><li>Logging events are automatically <em>buffered</em> by the
+        native TCP implementation. This means that if the link to server
+        is slow but still faster than the rate of (log) event production
+        by the client, the client will not be affected by the slow
+        network connection. However, if the network connection is slower
+        then the rate of event production, then the client can only
+        progress at the network rate. In particular, if the network link
+        to the the server is down, the client will be blocked.
+
+        <p>On the other hand, if the network link is up, but the server
+        is down, the client will not be blocked when making log requests
+        but the log events will be lost due to server unavailability.
+
+        <p><li>Even if a <code>SocketAppender</code> is no longer
+        attached to any category, it will not be garbage collected in
+        the presence of a connector thread. A connector thread exists
+        only if the connection to the server is down. To avoid this
+        garbage collection problem, you should #close the the
+        <code>SocketAppender</code> explicitly. See also next item.
+
+        <p>Long lived applications which create/destroy many
+        <code>SocketAppender</code> instances should be aware of this
+        garbage collection problem. Most other applications can safely
+        ignore it.
+
+        <p><li>If the JVM hosting the <code>SocketAppender</code> exits
+        before the <code>SocketAppender</code> is closed either
+        explicitly or subsequent to garbage collection, then there might
+        be untransmitted data in the pipe which might be lost. This is a
+        common problem on Windows based systems.
+
+        <p>To avoid lost data, it is usually sufficient to 
+        #close the <code>SocketAppender</code> either explicitly or by
+        calling the LogManager#shutdown method
+        before exiting the application.
+
+
+        </ul>
+        */
+        
+      	class XMLSocketAppender : public AppenderSkeleton
+    	{
+		class Connector;
+		friend class Connector;
+    	public:
+    		/**
+    		The default port number of remote logging server (4560).
+    		*/
+    		static int DEFAULT_PORT;
+
+    		/**
+    		The default reconnection delay (30000 milliseconds or 30 seconds).
+    		*/
+    		static int DEFAULT_RECONNECTION_DELAY;
+
+    	protected:
+    		/**
+    		host name
+    		*/
+    		tstring remoteHost;
+
+    		/**
+    		IP address
+    		*/
+    		helpers::InetAddress address;
+
+    		int port;
+    		helpers::SocketOutputStreamPtr os;
+    		int reconnectionDelay;
+    		bool locationInfo;
+			char zeroBuffer[1024];
+
+			/**
+			An event XML stream cannot exceed 1024 bytes.
+			*/
+			static const int MAX_EVENT_LEN;
+
+		public:
+    		XMLSocketAppender();
+			~XMLSocketAppender();
+
+    		/**
+    		Connects to remote server at <code>address</code> and <code>port</code>.
+    		*/
+    		XMLSocketAppender(unsigned long address, int port);
+
+    		/**
+    		Connects to remote server at <code>host</code> and <code>port</code>.
+    		*/
+    		XMLSocketAppender(const tstring& host, int port);
+
+    		/**
+    		Connect to the specified <b>RemoteHost</b> and <b>Port</b>.
+    		*/
+    		void activateOptions();
+
+		    /**
+		    Set options
+		    */
+			virtual void setOption(const tstring& option, const tstring& value);
+			
+    		/**
+    		* Close this appender.
+    		*
+    		* <p>This will mark the appender as closed and call then 
+    		* #cleanUp method.
+    		* */
+    		void close();
+
+    		/**
+    		* Drop the connection to the remote host and release the underlying
+    		* connector thread if it has been created
+    		* */
+    		void cleanUp();
+
+    		void connect();
+
+
+    		virtual void append(const spi::LoggingEvent& event);
+
+    		/**
+    		* The SocketAppender does not use a layout. Hence, this method
+    		* returns <code>false</code>.
+    		* */
+    		bool requiresLayout()
+    			{ return false; }
+
+    		/**
+    		* The <b>RemoteHost</b> option takes a string value which should be
+    		* the host name of the server where a 
+			* {@link net::SocketNode SocketNode} is running.
+    		* */
+    		inline void setRemoteHost(const tstring& host)
+    			{ address = helpers::InetAddress::getByName(host);
+    			remoteHost = host; }
+
+    		/**
+    		Returns value of the <b>RemoteHost</b> option.
+    		*/
+    		inline const tstring& getRemoteHost() const
+    			{ return remoteHost; }
+
+    		/**
+    		The <b>Port</b> option takes a positive integer representing
+    		the port where the server is waiting for connections.
+    		*/
+    		void setPort(int port)
+    			{ this->port = port; }
+
+    		/**
+    		Returns value of the <b>Port</b> option.
+    		*/
+    		int getPort() const
+    			{ return port; }
+
+    		/**
+    		The <b>LocationInfo</b> option takes a boolean value. If true,
+    		the information sent to the remote host will include location
+    		information. By default no location information is sent to the server.
+    		*/
+    		void setLocationInfo(bool locationInfo)
+    			{ this->locationInfo = locationInfo; }
+
+    		/**
+    		Returns value of the <b>LocationInfo</b> option.
+    		*/
+    		bool getLocationInfo() const
+    			{ return locationInfo; }
+
+    		/**
+    		The <b>ReconnectionDelay</b> option takes a positive integer
+    		representing the number of milliseconds to wait between each
+    		failed connection attempt to the server. The default value of
+    		this option is 30000 which corresponds to 30 seconds.
+
+    		<p>Setting this option to zero turns off reconnection
+    		capability.
+    		*/
+    		void setReconnectionDelay(int reconnectionDelay)
+    			{ this->reconnectionDelay = reconnectionDelay; }
+
+    		/**
+    		Returns value of the <b>ReconnectionDelay</b> option.
+    		*/
+    		int getReconnectionDelay() const
+    			{ return reconnectionDelay; }
+
+		    void fireConnector();
+       
+       private:
+		   /**
+			The Connector will reconnect when the server becomes available
+			again.  It does this by attempting to open a new connection every
+			<code>reconnectionDelay</code> milliseconds.
+
+			<p>It stops trying whenever a connection is established. It will
+			restart to try reconnect to the server when previpously open
+			connection is droppped.
+			*/
+			class Connector : public helpers::Thread, public helpers::ObjectImpl
+			{
+			public:
+				bool interrupted;
+				XMLSocketAppender * socketAppender;
+
+				Connector(XMLSocketAppenderPtr socketAppender);
+				virtual void run();
+			}; // class Connector
+
+			Connector * connector;
+        }; // class XMLSocketAppender
+    } // namespace net
+}; // namespace log4cxx
+
+#endif // _LOG4CXX_NET_XML_SOCKET_APPENDER_H
+
---
+++ src/xmlsocketappender.cpp
@@ -0,0 +1,280 @@
+/***************************************************************************
+                          xmlsocketappender.cpp  -  class XMLSocketAppender
+                             -------------------
+    begin                : jeu mai 8 2003
+    copyright            : (C) 2003 by Michael CATANZARITI
+    email                : mcatan@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.      *
+ *                                                                         *
+ * This software is published under the terms of the Apache Software       *
+ * License version 1.1, a copy of which has been included with this        *
+ * distribution in the LICENSE.txt file.                                   *
+ ***************************************************************************/
+
+#include <log4cxx/net/xmlsocketappender.h>
+#include <log4cxx/helpers/loglog.h>
+#include <log4cxx/helpers/socketoutputstream.h>
+#include <log4cxx/helpers/optionconverter.h>
+#include <log4cxx/helpers/stringhelper.h>
+#include <log4cxx/xml/xmllayout.h>
+#include <log4cxx/level.h>
+#include <log4cxx/helpers/transform.h>
+
+using namespace log4cxx;
+using namespace log4cxx::helpers;
+using namespace log4cxx::net;
+using namespace log4cxx::xml;
+
+// The default port number of remote logging server (4560)
+int XMLSocketAppender::DEFAULT_PORT                 = 4560;
+
+// The default reconnection delay (30000 milliseconds or 30 seconds).
+int XMLSocketAppender::DEFAULT_RECONNECTION_DELAY   = 30000;
+
+const int XMLSocketAppender::MAX_EVENT_LEN 				= 1024;
+
+XMLSocketAppender::XMLSocketAppender()
+: port(DEFAULT_PORT), reconnectionDelay(DEFAULT_RECONNECTION_DELAY), 
+locationInfo(false), connector(0)
+{
+	layout = new XMLLayout();
+	memset(zeroBuffer, 0, MAX_EVENT_LEN);
+}
+
+XMLSocketAppender::XMLSocketAppender(unsigned long address, int port)
+: port(port), reconnectionDelay(DEFAULT_RECONNECTION_DELAY), 
+locationInfo(false), connector(0)
+{
+	layout = new XMLLayout();
+	memset(zeroBuffer, 0, MAX_EVENT_LEN);
+	this->address.address = address;
+	remoteHost = this->address.getHostName();
+	connect();
+}
+
+XMLSocketAppender::XMLSocketAppender(const tstring& host, int port)
+: address(InetAddress::getByName(host)), port(port),
+reconnectionDelay(DEFAULT_RECONNECTION_DELAY), locationInfo(false),
+remoteHost(host), connector(0)
+{
+	layout = new XMLLayout();
+	memset(zeroBuffer, 0, MAX_EVENT_LEN);
+	connect();
+}
+
+XMLSocketAppender::~XMLSocketAppender()
+{
+	finalize();
+}
+
+void XMLSocketAppender::activateOptions()
+{
+	connect();
+}
+
+void XMLSocketAppender::setOption(const tstring& option,
+	const tstring& value)
+{
+	if (StringHelper::equalsIgnoreCase(option, _T("remotehost")))
+	{
+		setRemoteHost(value);
+	}
+	else if (StringHelper::equalsIgnoreCase(option, _T("port")))
+	{
+		setPort(OptionConverter::toInt(value, DEFAULT_PORT));
+	}
+	else if (StringHelper::equalsIgnoreCase(option, _T("locationinfo")))
+	{
+		setLocationInfo(OptionConverter::toBoolean(value, false));
+	}
+	else if (StringHelper::equalsIgnoreCase(option, _T("reconnectiondelay")))
+	{
+		setReconnectionDelay(OptionConverter::toInt(value, DEFAULT_RECONNECTION_DELAY));
+	}
+	else
+	{
+		AppenderSkeleton::setOption(name, value);
+	}
+}
+
+void XMLSocketAppender::close()
+{
+	synchronized sync(this);
+
+	if(closed)
+	{
+		return;
+	}
+
+	closed = true;
+	cleanUp();
+}
+
+void XMLSocketAppender::cleanUp()
+{
+	if(os != 0)
+	{
+		try
+		{
+			os->close();
+		}
+		catch(IOException& e)
+		{
+			LogLog::error(_T("Could not close socket :"), e);
+		}
+		
+		os = 0;
+	}
+	
+	if(connector != 0)
+	{
+		//LogLog::debug(_T("Interrupting the connector."));
+		connector->interrupted = true;
+		connector = 0;
+	}
+	
+}
+
+void XMLSocketAppender::connect()
+{
+	if(address.address == 0)
+	{
+		return;
+	}
+	
+	try
+	{
+		// First, close the previous connection if any.
+		cleanUp();
+		
+		SocketPtr socket = new Socket(address, port);
+		os = socket->getOutputStream();
+	}
+	catch(SocketException& e)
+	{
+		tstring msg = _T("Could not connect to remote log4cxx server at [")
+
+			+address.getHostName()+_T("].");
+			
+		if(reconnectionDelay > 0)
+		{
+			msg += _T(" We will try again later. ");
+		}
+
+		fireConnector(); // fire the connector thread
+		
+		LogLog::error(msg, e);
+	}
+}
+
+void XMLSocketAppender::append(const spi::LoggingEvent& event)
+{
+	if(address.address == 0)
+	{
+		errorHandler->error(
+			_T("No remote host is set for XMLSocketAppender named \"") +
+			name+_T("\"."));
+		return;
+	}
+
+	if(os != 0) try
+	{
+/*
+		if(locationInfo)
+		{
+			event.getLocationInformation();
+		}
+*/
+		tostringstream output;
+
+		this->layout->format(output, event);
+
+		tstring sz = output.str();
+		if (sz.length() <= MAX_EVENT_LEN)
+		{
+			USES_CONVERSION;
+			os->write((void *)T2A(sz.c_str()), sz.length());
+
+			if (MAX_EVENT_LEN - sz.length() > 0)
+			{
+				// complete with zeros up to MAX_EVENT_LEN characters
+				os->write(zeroBuffer, MAX_EVENT_LEN - sz.length());
+			}
+
+			// flush to socket
+			os->flush();
+		}
+	}
+	catch(SocketException& e)
+	{
+		os = 0;
+		LogLog::warn(_T("Detected problem with connection: "), e);
+
+		if(reconnectionDelay > 0)
+		{
+			fireConnector();
+		}
+
+	}
+}
+
+void XMLSocketAppender::fireConnector()
+{
+	if(connector == 0)
+	{
+		LogLog::debug(_T("Starting a new connector thread."));
+		connector = new Connector(this);
+		connector->setPriority(Thread::MIN_PRIORITY);
+		connector->start();
+	}
+}
+
+XMLSocketAppender::Connector::Connector(XMLSocketAppenderPtr socketAppender)
+: interrupted(false), socketAppender(socketAppender)
+{
+}
+
+void XMLSocketAppender::Connector::run()
+{
+	SocketPtr socket;
+	while(!interrupted)
+	{
+		try
+		{
+			sleep(socketAppender->reconnectionDelay);
+			LogLog::debug(_T("Attempting connection to ")
+				+socketAppender->address.getHostName());
+			socket = new Socket(socketAppender->address, socketAppender->port);
+			
+			synchronized sync(this);
+			{
+				socketAppender->os = socket->getOutputStream();
+				socketAppender->connector = 0;
+				LogLog::debug(_T("Connection established. Exiting connector thread."));
+				break;
+			}
+		}
+		catch(InterruptedException& e)
+		{
+			LogLog::debug(_T("Connector interrupted. Leaving loop."));
+			return;
+		}
+		catch(ConnectException& e)
+		{
+			LogLog::debug(_T("Remote host ")
+				+socketAppender->address.getHostName()
+				+_T(" refused connection."));
+		}
+		catch(IOException& e)
+		{
+			LogLog::debug(_T("Could not connect to ")
+				 +socketAppender->address.getHostName()
+				 +_T(". Exception is ") + e.getMessage());
+		}
+	}
+	
+	LogLog::debug(_T("Exiting Connector.run() method."));
+}
