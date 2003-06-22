fa6ae918eace5c413a6b9b9baa4a5a09be5bbf0b
Log Debug <log_debug@log_debug>
2003-06-22 16:42:51 +0000

    deleted as name was wrong - see htmllayout_test

--- tests/htmlappender_test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-./htmlappender_test > result
-cat result | sed 's/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]//' > htmlappender_test.result.stripped
-diff htmlappender_test.result.stripped htmlappender_test.witness
