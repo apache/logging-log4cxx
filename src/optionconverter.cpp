031f6ae8852540639f73b48e0f8aabe09d726086
Michael Catanzariti <mcatan@apache.org>
2003-06-28 10:42:24 +0000

    Properties class support

--- src/optionconverter.cpp
+++ src/optionconverter.cpp
@@ -18,7 +18,16 @@
 #include <algorithm>
 #include <ctype.h>
 #include <log4cxx/helpers/stringhelper.h>
+#include <log4cxx/helpers/exception.h>
+#include <stdlib.h>
+#include <log4cxx/helpers/properties.h>
+#include <log4cxx/helpers/loglog.h>
+#include <log4cxx/level.h>
+#include <log4cxx/helpers/object.h>
+#include <log4cxx/helpers/class.h>
+#include <log4cxx/helpers/loader.h>
 
+using namespace log4cxx;
 using namespace log4cxx::helpers;
 
 tstring OptionConverter::DELIM_START = _T("${");
@@ -100,4 +109,151 @@ long OptionConverter::toFileSize(const tstring& value, long dEfault)
 	return dEfault;
 }
 
+tstring OptionConverter::findAndSubst(const tstring& key, Properties& props)
+{
+	tstring value = props.getProperty(key);
 
+	if(value.empty())
+		return value;
+
+	try
+	{
+		return substVars(value, props);
+	}
+	catch(IllegalArgumentException& e)
+	{
+		LogLog::error(_T("Bad option value [")+value+_T("]."), e);
+		return value;
+	}
+}
+
+tstring OptionConverter::substVars(const tstring& val, Properties& props)
+{
+	tostringstream sbuf;
+
+	int i = 0;
+	int j, k;
+
+	while(true)
+	{
+		j = val.find(DELIM_START, i);
+		if(j == -1)
+		{
+			// no more variables
+			if(i==0)
+			{ // this is a simple string
+				return val;
+			}
+			else
+			{ // add the tail string which contails no variables and return the result.
+				sbuf << val.substr(i, val.length() - i);
+				return sbuf.str();
+			}
+		}
+		else
+		{
+			sbuf << val.substr(i, j - i);
+			k = val.find(DELIM_STOP, j);
+			if(k == -1)
+			{
+				tostringstream oss;
+				oss << _T("\"") << val
+					<< _T("\" has no closing brace. Opening brace at position ")
+					<< j << _T(".");
+				throw new IllegalArgumentException(oss.str());
+			}
+			else
+			{
+				j += DELIM_START_LEN;
+				tstring key = val.substr(j, k - j);
+				// first try in System properties
+				tstring replacement = getSystemProperty(key, _T(""));
+				// then try props parameter
+				if(replacement.empty())
+				{
+					replacement = props.getProperty(key);
+				}
+
+				if(!replacement.empty())
+				{
+					// Do variable substitution on the replacement string
+					// such that we can solve "Hello ${x2}" as "Hello p1"
+					// the where the properties are
+					// x1=p1
+					// x2=${x1}
+					tstring recursiveReplacement = substVars(replacement, props);
+					sbuf << (recursiveReplacement);
+				}
+				i = k + DELIM_STOP_LEN;
+			}
+		}
+	}
+}
+
+tstring OptionConverter::getSystemProperty(const tstring& key, const tstring& def)
+{
+	if (!key.empty())
+	{
+		USES_CONVERSION;
+		tstring value = A2T(::getenv(T2A(def.c_str())));
+
+		if (!value.empty())
+		{
+			return value;
+		}
+		else
+		{
+			return def;
+		}
+	}
+	else
+	{
+		return def;
+	}
+}
+
+const Level& OptionConverter::toLevel(const tstring& value, const Level& defaultValue)
+{
+	return Level::toLevel(value, defaultValue);
+}
+
+
+ObjectPtr OptionConverter::instantiateByKey(Properties& props, const tstring& key,
+	const Class& superClass, ObjectPtr defaultValue)
+{
+// Get the value of the property in string form
+	tstring className = findAndSubst(key, props);
+	if(className.empty())
+	{
+		LogLog::error(_T("Could not find value for key ") + key);
+		return defaultValue;
+	}
+	// Trim className to avoid trailing spaces that cause problems.
+	return OptionConverter::instantiateByClassName(
+		StringHelper::trim(className), superClass, defaultValue);
+}
+
+ObjectPtr OptionConverter::instantiateByClassName(const tstring& className,
+	const Class& superClass, ObjectPtr defaultValue)
+{
+	if(!className.empty())
+	{
+		try
+		{
+			const Class& classObj = Loader::loadClass(className);
+			ObjectPtr newObject =  classObj.newInstance();
+			if (!newObject->instanceof(superClass))
+			{
+				return defaultValue;
+			}
+
+			return newObject;
+		}
+		catch (Exception& e)
+		{
+			LogLog::error(_T("Could not instantiate class [") + className
+				+ _T("]."), e);
+		}
+	}
+	return defaultValue;
+}
