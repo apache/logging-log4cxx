ac2b95aa6e41eeede31a97aa1a81906ecf58d4a4
Michael Catanzariti <mcatan@apache.org>
2003-07-21 23:12:51 +0000

    updated consoleappender_test

--- tests/consoleappender_test.cpp
+++ tests/consoleappender_test.cpp
@@ -3,33 +3,99 @@
 #include <log4cxx/consoleappender.h>
 #include <log4cxx/simplelayout.h>
 #include <log4cxx/helpers/exception.h>
+#include <fstream>
 
 using namespace log4cxx;
 using namespace log4cxx::helpers;
 
 int main()
 {
-  int result = EXIT_SUCCESS;
+	int ret = EXIT_SUCCESS;
+	
+	try
+	{
+    	LayoutPtr layout = new SimpleLayout();
+    	AppenderPtr consoleAppender =
+      		new ConsoleAppender(layout, _T("System.Out"));
 
-  try
-  {
-    LayoutPtr layout = new SimpleLayout();
-    AppenderPtr consoleAppender =
-      new ConsoleAppender(layout, _T("System.Out"));
+		LoggerPtr rootLogger = Logger::getRootLogger();
+		rootLogger->addAppender(consoleAppender);
+		
+		// redirecting stdout to file "result"
+		freopen("result", "w", stdout);
 
-    LoggerPtr rootLogger = Logger::getRootLogger();
-    rootLogger->addAppender(consoleAppender);
+		rootLogger->debug(_T("debug message"));
+		rootLogger->info(_T("info message"));
+		rootLogger->warn(_T("warn message"));
+		rootLogger->error(_T("error message"));
+		rootLogger->fatal(_T("fatal message"));
 
-    rootLogger->debug(_T("debug message"));
-    rootLogger->info(_T("info message"));
-    rootLogger->warn(_T("warn message"));
-    rootLogger->error(_T("error message"));
-    rootLogger->fatal(_T("fatal message"));
-  }
-  catch(Exception&)
-  {
-    result = EXIT_FAILURE;
-  }
+		consoleAppender->close();
 
-  return result;
+#ifdef UNICODE
+		std::wofstream witness;
+#else
+		std::ofstream witness;
+#endif
+		witness.open("witness", std::ios::out|std::ios::trunc);
+		witness << _T("DEBUG - debug message") << std::endl;
+		witness << _T("INFO - info message") << std::endl;
+		witness << _T("WARN - warn message") << std::endl;
+		witness << _T("ERROR - error message") << std::endl;
+		witness << _T("FATAL - fatal message") << std::endl;
+		witness.close();
+
+#ifdef UNICODE
+		std::wifstream inr, inw;
+#else
+		std::ifstream inr, inw;
+#endif
+		inr.open("result", std::ios::in|std::ios::binary);
+		inw.open("witness", std::ios::in|std::ios::binary);
+		if (inr.fail() || inw.fail())
+		{
+			ret = EXIT_FAILURE;
+		}
+		else
+		{
+			inr.seekg(0, std::ios_base::end);
+			int sizer = inr.tellg();
+			inr.seekg(0, std::ios_base::beg);
+
+			inw.seekg(0, std::ios_base::end);
+			int sizew = inw.tellg();
+			inw.seekg(0, std::ios_base::beg);
+
+			if (sizer != sizew)
+			{
+				ret = EXIT_FAILURE;
+			}
+			else
+			{
+				unsigned char * result = new unsigned char[sizer];
+				unsigned char * witness = new unsigned char[sizer];
+				memset(result, (sizer) * sizeof(TCHAR), 0);
+				memset(witness, (sizer) * sizeof(TCHAR), 0);
+				inr.read((TCHAR *)result, sizer);
+				inw.read((TCHAR *)witness, sizer);
+
+				if (memcmp(result, witness, sizer) != 0)
+				{
+					ret = EXIT_FAILURE;
+				}
+
+				delete [] result;
+				delete [] witness;
+
+				inr.close();
+				inw.close();
+			}
+		}
+	}
+	catch(Exception&)
+	{
+		ret = EXIT_FAILURE;
+	}
+
+	return ret;
 }
--- tests/consoleappender_test.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-./consoleappender_test > result && diff result consoleappender_test.witness
