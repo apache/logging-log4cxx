3e6b34672b2cf3f334ae0fb492914b964409fc90
Michael Catanzariti <mcatan@apache.org>
2004-01-18 16:27:03 +0000

    new examples

---
+++ examples/trivial.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <log4cxx/logger.h>
+#include <log4cxx/basicconfigurator.h>
+#include <log4cxx/helpers/exception.h>
+#include <log4cxx/ndc.h>
+
+using namespace log4cxx;
+using namespace log4cxx::helpers;
+
+int main()
+{
+    int result = EXIT_SUCCESS;
+    try
+    {
+		BasicConfigurator::configure();
+ 		LoggerPtr rootLogger = Logger::getRootLogger();
+
+		NDC::push("trivial context");
+
+		rootLogger->debug(_T("debug message"));
+		rootLogger->info(_T("info message"));
+		rootLogger->warn(_T("warn message"));
+		rootLogger->error(_T("error message"));
+		rootLogger->fatal(_T("fatal message"));
+	}
+	catch(Exception&)
+	{
+		result = EXIT_FAILURE;
+	}
+
+    return result;
+}
