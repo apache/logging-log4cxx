1fe178b31bdd9b55610bd1910c2b9211c76f4736
Michael Catanzariti <mcatan@apache.org>
2004-01-27 21:37:19 +0000

    tests can be passes in release, unicode debug, and unicode release

--- msvc/tests/runtests.bat
+++ msvc/tests/runtests.bat
@@ -1,52 +1,62 @@
 @echo off
 
+if %1/==/ goto usage
+if not %1 == debug if not %1 == release if not %1 == unicode_d if not %1 == unicode_r goto usage
+
 cd ..\..\tests
 
 echo ********** Minimum **********
 call ..\msvc\tests\clean.bat
-..\msvc\bin\debug\testsuite MinimumTestCase
+..\msvc\bin\%1\testsuite MinimumTestCase
 
 echo ********** Logger **********
 call ..\msvc\tests\clean.bat
-..\msvc\bin\debug\testsuite LoggerTestCase
+..\msvc\bin\%1\testsuite LoggerTestCase
 
 echo ********** PatternLayout **********
 call ..\msvc\tests\clean.bat
-..\msvc\bin\debug\testsuite PatternLayoutTest
+..\msvc\bin\%1\testsuite PatternLayoutTest
 
 echo ********** HierarchyThreshold **********
 call ..\msvc\tests\clean.bat
-..\msvc\bin\debug\testsuite HierarchyThresholdTestCase
+..\msvc\bin\%1\testsuite HierarchyThresholdTestCase
 
 echo ********** CustomLogger **********
 call ..\msvc\tests\clean.bat
-..\msvc\bin\debug\testsuite XLoggerTestCase
+..\msvc\bin\%1\testsuite XLoggerTestCase
 
 echo ********** DefaultInit **********
 call ..\msvc\tests\clean.bat
-..\msvc\bin\debug\testsuite TestCase1
+..\msvc\bin\%1\testsuite TestCase1
 copy input\xml\defaultInit.xml log4j.xml
-..\msvc\bin\debug\testsuite TestCase2
+..\msvc\bin\%1\testsuite TestCase2
 del log4j.xml
 copy input\defaultInit3.properties log4j.properties
-..\msvc\bin\debug\testsuite TestCase3
+..\msvc\bin\%1\testsuite TestCase3
 del log4j.properties
 copy input\defaultInit3.properties log4j.properties
 copy input\xml\defaultInit.xml log4j.xml
-..\msvc\bin\debug\testsuite TestCase4
+..\msvc\bin\%1\testsuite TestCase4
 
 echo ********** AsyncAppender **********
 call ..\msvc\tests\clean.bat
-..\msvc\bin\debug\testsuite AsyncAppenderTestCase
+..\msvc\bin\%1\testsuite AsyncAppenderTestCase
 
 echo ********** BoundedFIFO **********
 call ..\msvc\tests\clean.bat
-..\msvc\bin\debug\testsuite BoundedFIFOTestCase
+..\msvc\bin\%1\testsuite BoundedFIFOTestCase
 
 echo ********** CyclicBuffer **********
 call ..\msvc\tests\clean.bat
-..\msvc\bin\debug\testsuite CyclicBufferTestCase
+..\msvc\bin\%1\testsuite CyclicBufferTestCase
 
 
 cd ..\msvc\tests
 
+goto done
+
+:usage
+echo Usage : runtests.bat TARGET 
+echo where TARGET can be fixed to debug, release, unicode_d or unicode_r.
+
+:done
