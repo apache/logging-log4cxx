6b093adddea570f0379160d6cd54d61e3a1117fa
Michael Catanzariti <mcatan@apache.org>
2004-01-27 22:27:21 +0000

    added license header

--- examples/trivial.cpp
+++ examples/trivial.cpp
@@ -1,3 +1,11 @@
+/***************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.      *
+ *                                                                         *
+ * This software is published under the terms of the Apache Software       *
+ * License version 1.1, a copy of which has been included with this        *
+ * distribution in the LICENSE.txt file.                                   *
+ ***************************************************************************/
+
 #include <stdlib.h>
 #include <log4cxx/logger.h>
 #include <log4cxx/basicconfigurator.h>
