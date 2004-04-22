011e42cb2a18edd936988138e1c0a4c6c2437992
Michael Catanzariti <mcatan@apache.org>
2004-04-22 21:21:38 +0000

    updated license to version 2.0

--- src/appenderskeleton.cpp
+++ src/appenderskeleton.cpp
@@ -1,19 +1,18 @@
-/***************************************************************************
-                          appenderskeleton.cpp  -  AppenderSkeleton
-                             -------------------
-    begin                : mar avr 15 2003
-    copyright            : (C) 2003 by michael
-    email                : michael@montmartre-2-81-57-89-54.fbx.proxad.net
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*
+ * Copyright 2003,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <log4cxx/spi/loggingevent.h>
 #include <log4cxx/appenderskeleton.h>
--- src/serversocket.cpp
+++ src/serversocket.cpp
@@ -1,20 +1,19 @@
-/***************************************************************************
-                          serversocket.cpp  -  ServerSocket
-                             -------------------
-    begin                : ven mai 9 2003
-    copyright            : (C) 2003 by Michael CATANZARITI
-    email                : mcatan@free.fr
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
+/*
+ * Copyright 2003,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 #include <log4cxx/config.h>
 
 #ifdef WIN32
