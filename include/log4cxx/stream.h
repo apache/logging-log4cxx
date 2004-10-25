47c13ae9366e66659bd990190ed27e9d5bc08e01
Curt Arnold <carnold@apache.org>
2004-10-25 04:37:33 +0000

    LOGCXX-18: Reworked as std::ios_base to reduce overhead when disabled

--- include/log4cxx/stream.h
+++ include/log4cxx/stream.h
@@ -23,170 +23,110 @@
 
 namespace log4cxx
 {
-	/**
-	 * Implements an STL streambuf for use by logging streams.
-	 */
-	template <class Elem, class Tr = ::std::char_traits<Elem> >
-	   class basic_logstreambuf : public ::std::basic_stringbuf<Elem, Tr> {
-	   public:
-	   /**
-	    * Constructor.
-	    *
-	    */
-	   basic_logstreambuf(const ::log4cxx::LoggerPtr& logger,
-	                   const ::log4cxx::LevelPtr& level) :
-	                   ::std::basic_stringbuf<Elem, Tr>(::std::ios_base::out),
-	                   logger(logger),
-	                   level(level)  {
-	        enabled = logger->isEnabledFor(level);
-	   }
+        /**
+         * This template acts as a proxy for base_logstreamimpl
+         * and defers the potentially expensive construction
+         * of basic_stream until needed.
+         */
+        template <class Elem, class Tr = ::std::char_traits<Elem> >
+           class basic_logstream : public ::std::ios_base {
+             public:
+             /**
+              * Constructor.
+              */
+             basic_logstream(const ::log4cxx::LoggerPtr& loggr,
+                          const ::log4cxx::LevelPtr& level) :
+                          logger(loggr),
+                          currentLevel(level),
+                          impl(0),
+                          enabled(loggr->isEnabledFor(level)) {
+             }
 
-       /**
-        * Gets whether logger is currently enabled for the specified level.
-        * @returns true if enabled
-        */
-	   inline bool isEnabled() const {
-	   	  return enabled;
-	   }
+            /**
+             * Constructor.
+             */
+             basic_logstream(const char* logName,
+               ::log4cxx::LevelPtr& level) :
+               logger(::log4cxx::Logger::getLogger(logName)),
+               currentLevel(level),
+               impl(0),
+               enabled(logger->isEnabledFor(level)) {
+              }
 
-	   /**
-	    * Sets the call site location.
-	    * @param location call site location
-	    */
-	   void setLocation(const ::log4cxx::spi::location::LocationInfo& location) {
-	   	   this->location = location;
-	   }
-
-	   /**
-	    * Sets the level.
-	    * @param level level
-	    */
-	   void setLevel(const ::log4cxx::LevelPtr& level) {
-	   	  bool newState = logger->isEnabledFor(level);
-	   	  //
-	   	  //    if not previously enabled but soon to be enabled then
-	   	  //       reset the buffer to clear any previously inserted content
-	   	  if (newState && !enabled) {
-	   	     //
-	   	     //   reset the stream buffer
-	   	     seekoff(0, ::std::ios_base::beg, ::std::ios_base::out);
-	   	  }
-	   	  this->level = level;
-	   	  enabled = newState;
-	   }
-
-       protected:
-       /**
-        * Synchronizes the stream and logger.
-        *
-        */
-	   int sync() {
-	   	  //
-	   	  //  if previously enabled
-	   	  if (enabled) {
-	   	  	 //  check (and cache) whether it is still enabled
-	   	     enabled = logger->isEnabledFor(level);
-	   	     //  log message if still enabled
-	   	     if (enabled) {
-	   	          logger->forcedLog(level,
-	   	              str(),
-	   	              location.getFileName(),
-	   	              location.getLineNumber());
-	   	     }
-	   	  }
-	   	  //  clear call site information
-          location.clear();
-	   	  //
-	   	  //   reset the stream buffer
-	   	  seekoff(0, ::std::ios_base::beg, ::std::ios_base::out);
-	   	  return 0;
-	   }
-
-       bool isEnabledFor(const ::log4cxx::LevelPtr& level) const {
-       	   return logger.isEnabledFor(level);
-       }
-
-	   private:
-	   /**
-	    * logger.
-	    */
-	   ::log4cxx::LoggerPtr logger;
-	   /**
-	    * level.
-	    */
-	   ::log4cxx::LevelPtr level;
-	   /**
-	    * location.
-	    */
-	   ::log4cxx::spi::location::LocationInfo location;
-
-       /**
-        * State of logger at last sync or level changes.
-        */
-       bool enabled;
-
-	};
-
-	/**
-	 * This template provides an stream interface layer to
-	 * log4cxx.
-	 */
-	template <class Elem, class Tr = ::std::char_traits<Elem> >
-	   class basic_logstream : public ::std::basic_ostream<Elem, Tr> {
-
-	   public:
-	   /**
-	    * Constructor.
-	    */
-	   basic_logstream(const ::log4cxx::LoggerPtr& logger,
-	                const ::log4cxx::LevelPtr& level) :
-	                ::std::basic_ostream<Elem, Tr>(&buffer),
-	                buffer(logger, level) {
-	   }
-
-          /**
-           * Constructor.
-           */
-           basic_logstream(const char* logName,
-             ::log4cxx::LevelPtr& level) :
-             ::std::basic_ostream<Elem, Tr>(&buffer),
-             buffer(::log4cxx::Logger::getLogger(logName), level) {
-            }
+              ~basic_logstream() {
+                  delete impl;
+              }
 
 
-          /**
-           * Sets the call site location.
-           * @param location call site location
-           */
-	   void setLocation(const ::log4cxx::spi::location::LocationInfo& location) {
-	   	   buffer.setLocation(location);
-	   }
+
+             /**
+              * Set the level.
+              * @param level level
+              */
+              void setLevel(const ::log4cxx::LevelPtr& level) {
+                  currentLevel = level;
+                  enabled = logger->isEnabledFor(currentLevel);
+              }
+
+              inline bool isEnabled() const {
+                 return enabled;
+              }
+
+              bool isEnabledFor(const ::log4cxx::LevelPtr& level) const {
+                 return logger.isEnabledFor(level);
+              }
 
 
-	   /**
-	    * Set the level.
-	    * @param level level
-	    */
-	   void setLevel(const ::log4cxx::LevelPtr& level) {
-	   	  buffer.setLevel(level);
-	   }
-
-       inline bool isEnabled() const {
-           return buffer.isEnabled();
-       }
-       
-       bool isEnabledFor(const ::log4cxx::LevelPtr& level) const {
-       	   return buffer.isEnabledFor(level);
-       }
+              void setLocation(const ::log4cxx::spi::location::LocationInfo& location) {
+                 if (LOG4CXX_UNLIKELY(enabled)) {
+                    currentLocation = location;
+                 }
+              }
 
 
-	   private:
-	   basic_logstreambuf<Elem, Tr> buffer;
-	};
+
+             void flush(const ::log4cxx::spi::location::LocationInfo& location) {
+                 if (LOG4CXX_UNLIKELY(enabled && 0 != impl)) {
+                    ::std::basic_string<Elem, Tr> msg(impl->str());
+                    logger->log(currentLevel,
+                       msg,
+                       location.getFileName(),
+                       location.getLineNumber());
+                    msg.clear();
+                    impl->str(msg);
+                }
+             }
+
+             inline void flush() {
+                flush(currentLocation);
+             }
+
+
+             ::std::basic_ostream<Elem, Tr>& getStream() {
+                if (0 == impl) {
+                  impl = new ::std::basic_ostringstream<Elem, Tr>();
+                }
+                impl->precision(precision());
+                impl->width(width());
+                impl->flags(flags());
+                return *impl;
+             }
+
+             private:
+             LevelPtr currentLevel;
+             LoggerPtr logger;
+             ::log4cxx::spi::location::LocationInfo currentLocation;
+             bool enabled;
+             ::std::basic_ostringstream<Elem, Tr>* impl;
+
+        };
 
 	typedef basic_logstream<char> logstream;
 	typedef basic_logstream<wchar_t> wlogstream;
 }  // namespace log4cxx
+
+
+
 
 /**
 * Insertion operator for LocationInfo.
@@ -196,9 +136,7 @@ template<class Elem, class Tr>
 ::log4cxx::basic_logstream<Elem, Tr>& operator<<(
    ::log4cxx::basic_logstream<Elem, Tr>& lhs,
    const ::log4cxx::spi::location::LocationInfo& rhs) {
-   if (LOG4CXX_UNLIKELY(lhs.isEnabled())) {
-      lhs.setLocation(rhs);
-   }
+   lhs.setLocation(rhs);
    return lhs;
 }
 
@@ -210,16 +148,13 @@ template<class Elem, class Tr>
 ::log4cxx::basic_logstream<Elem, Tr>& operator<<(
    ::log4cxx::basic_logstream<Elem, Tr>& lhs,
    const ::log4cxx::spi::location::LocationFlush& rhs) {
-   if (LOG4CXX_UNLIKELY(lhs.isEnabled())) {
-   	  lhs.setLocation(rhs);
-   	  lhs.flush();
-   }
+   lhs.flush(rhs);
    return lhs;
 }
 
 
 /**
-* Insertion operator for LocationInfo.
+* Insertion operator for Level.
 *
 */
 template<class Elem, class Tr>
@@ -231,50 +166,32 @@ template<class Elem, class Tr>
 }
 
 
-#define LOG4CXX_STREAM_DEFINE_INSERTION(InsType)           \
-template<class Elem, class Tr>                             \
-::log4cxx::basic_logstream<Elem, Tr>& operator<<(          \
-   ::log4cxx::basic_logstream<Elem, Tr>& lhs,              \
-   InsType rhs) {                                          \
-   if (LOG4CXX_UNLIKELY(lhs.isEnabled())) {                \
-      ((::std::basic_ostream<Elem, Tr>&) lhs) << rhs;      \
-   }                                                       \
-   return lhs;                                             \
-}
-
-
-
-/*
-* Insertion operators for common types.
-* Can't use template or would get ambiguities.
-* If attempting to insert a type without a matching
-* logstream specific insertion operator, the type
-* will be formatted, but may get discarded.
-*
-*/
-LOG4CXX_STREAM_DEFINE_INSERTION(bool)
-LOG4CXX_STREAM_DEFINE_INSERTION(signed char)
-LOG4CXX_STREAM_DEFINE_INSERTION(unsigned char)
-LOG4CXX_STREAM_DEFINE_INSERTION(signed short)
-LOG4CXX_STREAM_DEFINE_INSERTION(unsigned short)
-LOG4CXX_STREAM_DEFINE_INSERTION(signed int)
-LOG4CXX_STREAM_DEFINE_INSERTION(unsigned int)
-LOG4CXX_STREAM_DEFINE_INSERTION(signed long)
-LOG4CXX_STREAM_DEFINE_INSERTION(unsigned long)
-LOG4CXX_STREAM_DEFINE_INSERTION(float)
-LOG4CXX_STREAM_DEFINE_INSERTION(double)
-LOG4CXX_STREAM_DEFINE_INSERTION(const Elem*)
-
+//
+//
+//   template for manipulators (std::scientific et al)
+//
 template<class Elem, class Tr>
 ::log4cxx::basic_logstream<Elem, Tr>& operator<<(
    ::log4cxx::basic_logstream<Elem, Tr>& lhs,
-   const ::std::basic_string<Elem, Tr>& rhs) {
-   if (LOG4CXX_UNLIKELY(lhs.isEnabled())) {
-      ((::std::basic_ostream<Elem, Tr>&) lhs) << rhs;
-   }
+   ::std::ios_base& (*manip)(::std::ios_base&)) {
+     (*manip)(lhs);
    return lhs;
 }
 
+
+
+//
+//   template for all other insertion operators
+//
+template<class Elem, class Tr, class ArbitraryType>
+::log4cxx::basic_logstream<Elem, Tr>& operator<<(
+   ::log4cxx::basic_logstream<Elem, Tr>& lhs,
+   const ArbitraryType& rhs) {
+   if (LOG4CXX_UNLIKELY(lhs.isEnabled())) {
+       lhs.getStream() << rhs;
+   }
+   return lhs;
+}
 
 #if !defined(LOG4CXX_ENDMSG)
 #define LOG4CXX_ENDMSG LOG4CXX_LOCATION_FLUSH
---
+++ tests/src/streamtestcase.cpp
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2003,2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <log4cxx/stream.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "vectorappender.h"
+#include <log4cxx/logmanager.h>
+#include <log4cxx/simplelayout.h>
+#include <log4cxx/spi/loggingevent.h>
+
+using namespace log4cxx;
+using namespace log4cxx::helpers;
+
+class ExceptionOnInsert {
+public:
+   ExceptionOnInsert() {
+   }
+};
+
+//
+//   define an insertion operation that will throw an
+//       exception to test that evaluation was short
+//       circuited
+//
+template<class Elem, class Tr>
+::std::basic_ostream<Elem, Tr>& operator<<(
+   ::std::basic_ostream<Elem, Tr>& lhs,
+   const ExceptionOnInsert& rhs) {
+   throw "Should have been short-circuited";
+}
+
+
+/**
+   Unit tests for the optional stream-like interface for log4cxx
+ */
+class StreamTestCase : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE(StreamTestCase);
+                CPPUNIT_TEST(testSimple);
+                CPPUNIT_TEST(testSimpleWithFlush);
+                CPPUNIT_TEST(testSimpleWithoutFlush);
+                CPPUNIT_TEST(testMultiple);
+                CPPUNIT_TEST(testShortCircuit);
+                CPPUNIT_TEST(testScientific);
+                CPPUNIT_TEST(testPrecision);
+//                CPPUNIT_TEST(testWidth);
+                CPPUNIT_TEST(testGetStream);
+                CPPUNIT_TEST(testGetStreamDebug);
+	CPPUNIT_TEST_SUITE_END();
+
+        VectorAppenderPtr vectorAppender;
+
+public:
+	void setUp() {
+           LoggerPtr root(Logger::getRootLogger());
+           LayoutPtr layout(new SimpleLayout());
+           vectorAppender = new VectorAppender();
+           root->addAppender(vectorAppender);
+        }
+
+	void tearDown()
+	{
+            LogManager::shutdown();
+	}
+
+        void testSimple() {
+            LoggerPtr root(Logger::getRootLogger());
+            log4cxx::logstream stream(root, log4cxx::Level::INFO);
+            stream << "This is a test" << LOG4CXX_ENDMSG;
+            CPPUNIT_ASSERT_EQUAL((size_t) 1, vectorAppender->getVector().size());
+        }
+
+        void testSimpleWithFlush() {
+           LoggerPtr root(Logger::getRootLogger());
+           log4cxx::logstream stream(root, log4cxx::Level::INFO);
+           stream << "This is a test\n";
+           stream.flush();
+           CPPUNIT_ASSERT_EQUAL((size_t) 1, vectorAppender->getVector().size());
+        }
+
+        void testSimpleWithoutFlush() {
+          LoggerPtr root(Logger::getRootLogger());
+          log4cxx::logstream stream(root, log4cxx::Level::INFO);
+          stream << "This is a test\n";
+          CPPUNIT_ASSERT_EQUAL((size_t) 0, vectorAppender->getVector().size());
+        }
+
+        void testMultiple() {
+           LoggerPtr root(Logger::getRootLogger());
+           log4cxx::logstream stream(root, log4cxx::Level::INFO);
+           stream << "This is a test" << LOG4CXX_ENDMSG;
+           stream << "This is another test message" << LOG4CXX_ENDMSG;
+           CPPUNIT_ASSERT_EQUAL((size_t) 2, vectorAppender->getVector().size());
+       }
+
+       void testShortCircuit() {
+         LoggerPtr logger(Logger::getLogger("StreamTestCase.shortCircuit"));
+         logger->setLevel(log4cxx::Level::INFO);
+         log4cxx::logstream stream(logger, log4cxx::Level::DEBUG);
+         ExceptionOnInsert someObj;
+         stream << someObj << LOG4CXX_ENDMSG;
+         CPPUNIT_ASSERT_EQUAL((size_t) 0, vectorAppender->getVector().size());
+       }
+
+       void testScientific() {
+           LoggerPtr root(Logger::getRootLogger());
+           log4cxx::logstream stream(root, log4cxx::Level::INFO);
+           stream //<< std::scientific
+                  << 0.000001115
+                  << LOG4CXX_ENDMSG;
+           spi::LoggingEventPtr event(vectorAppender->getVector()[0]);
+           std::string msg(event->getMessage());
+           CPPUNIT_ASSERT(msg.find("e-") != std::string::npos ||
+                msg.find("E-") != std::string::npos);
+       }
+
+       void testPrecision() {
+          LoggerPtr root(Logger::getRootLogger());
+          log4cxx::logstream stream(root, log4cxx::Level::INFO);
+          stream.precision(4);
+          stream << std::fixed
+              << 1.000001
+              << LOG4CXX_ENDMSG;
+          spi::LoggingEventPtr event(vectorAppender->getVector()[0]);
+          std::string msg(event->getMessage());
+          CPPUNIT_ASSERT(msg.find("1.00000") == std::string::npos);
+      }
+
+
+      void testWidth() {
+          LoggerPtr root(Logger::getRootLogger());
+          log4cxx::logstream stream(root, log4cxx::Level::INFO);
+          stream.width(4);
+          stream << std::fixed
+             << 10.00015
+             << LOG4CXX_ENDMSG;
+          spi::LoggingEventPtr event(vectorAppender->getVector()[0]);
+          std::string msg(event->getMessage());
+          CPPUNIT_ASSERT(msg.find("10.00") == std::string::npos);
+       }
+
+       void addMessage(std::ostream& os) {
+          os << "Hello, World";
+       }
+
+       void testGetStream() {
+         LoggerPtr root(Logger::getRootLogger());
+         log4cxx::logstream stream(root, log4cxx::Level::INFO);
+         addMessage(stream.getStream());
+         stream << LOG4CXX_ENDMSG;
+         spi::LoggingEventPtr event(vectorAppender->getVector()[0]);
+         std::string msg(event->getMessage());
+         CPPUNIT_ASSERT(msg.find("Hello, World") >= 0);
+       }
+
+
+       void testGetStreamDebug() {
+          LoggerPtr logger(Logger::getLogger("StreamTestCase.getStreamDebug"));
+          logger->setLevel(log4cxx::Level::INFO);
+          log4cxx::logstream stream(logger, log4cxx::Level::DEBUG);
+          addMessage(stream.getStream());
+          stream << LOG4CXX_ENDMSG;
+          CPPUNIT_ASSERT_EQUAL((size_t) 0, vectorAppender->getVector().size());
+       }
+
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(StreamTestCase);
