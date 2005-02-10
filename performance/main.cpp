d231afbb78275a1c454847fdb47992b66853d0a5
Curt Arnold <carnold@apache.org>
2005-02-10 05:07:31 +0000

    LOGCXX-57: Rework performance test as port of log4j's

--- performance/main.cpp
+++ performance/main.cpp
@@ -14,142 +14,178 @@
  * limitations under the License.
  */
 
-#include <log4cxx/ndc.h>
-#include <log4cxx/logmanager.h>
 #include <log4cxx/logger.h>
-#include <log4cxx/helpers/exception.h>
-#include <log4cxx/helpers/optionconverter.h>
-#include <log4cxx/xml/domconfigurator.h>
-#include <apr_general.h>
-#include <apr_pools.h>
+#include <apr.h>
 #include <apr_time.h>
 #include <iostream>
-
-#define _T(str) str
+#include <log4cxx/helpers/transcoder.h>
+#include <log4cxx/stream.h>
+#include <log4cxx/appender.h>
+#include <sstream>
+#include "nullappender.h"
+#include <log4cxx/consoleappender.h>
+#include <log4cxx/patternlayout.h>
+#include <log4cxx/helpers/pool.h>
+#include <log4cxx/xml/domconfigurator.h>
+#include <log4cxx/stream.h>
 
 using namespace log4cxx;
 using namespace log4cxx::helpers;
-
-int runLength;
-int delay = -1;
-/*
- A delay is applied after every <code>burstLen</code> log
- requests.  The default value of this constant is 100.  */
-int burstLen = 100;
-int DELAY_MULT = 1000/burstLen;
-
-LoggerPtr logger = Logger::getLogger(_T("A0123456789.B0123456789.C0123456789"));
-
-void Usage(const std::string& processName, const std::string& msg)
-{
-        std::cerr << msg << std::endl;
-        std::cerr <<
-                "Usage: " << processName
-                << " confFile runLength [delay] [burstLen]" << std::endl
-                << "       confFile is an XML configuration file and" << std::endl
-                << "       runLength (integer) is the length of test loop." << std::endl
-                << "       delay is the time in millisecs to wait every burstLen log requests." << std::endl;
-        exit(EXIT_FAILURE);
-}
+using namespace log4cxx::performance;
+using namespace log4cxx::xml;
 
 
-void init(const std::string& configFile, const std::string& runLengthStr,
-                  const std::string& delayStr, const std::string& burstLenStr)
-{
-        runLength = atoi(runLengthStr.c_str());
-        if (runLength < 1)
-        {
-                throw IllegalArgumentException("run Length must be greater than 0");
-        }
-        if (!delayStr.empty())
-        {
-                delay = atoi(delayStr.c_str());
-        }
-        if (!burstLenStr.empty())
-        {
-                burstLen = atoi(burstLenStr.c_str());
-                DELAY_MULT = 1000/burstLen;
-        }
 
-#ifdef LOG4CXX_HAVE_XML
-        xml::DOMConfigurator::configure(configFile);
-#endif
-}
+/**
+ * Logs in a loop a number of times and measure the elapsed time.
+ *
+ * @author Ceki G&uuml;lc&uuml;
+ */
+class Loop {
+  static int runLength;
+  static LoggerPtr logger;
+  typedef long (*loopFunc)(long,LoggerPtr&);
+  static loopFunc loop;
+public:
 
-double NoDelayLoop(LoggerPtr logger, const std::string& msg)
-{
-    log4cxx_time_t before = apr_time_now();
-    for(int i = 0; i < runLength; i++) {
-        logger->info(msg, LOG4CXX_LOCATION);
+  static void main(std::vector<LogString>& args)  {
+    LoggerPtr j(Logger::getLogger("org.apache.log4j.joran"));
+    j->setAdditivity(false);
+    j->setLevel(Level::WARN);
+    AppenderPtr a(new ConsoleAppender());
+    a->setLayout(new PatternLayout(LOG4CXX_STR("%d %level %c - %m%n")));
+    a->setName(LOG4CXX_STR("console"));
+    Pool p;
+    a->activateOptions(p);
+    j->addAppender(a);
+
+    if (args.size() >= 2) {
+      init(args);
+    } else {
+      usage("Wrong number of arguments.");
     }
-    log4cxx_time_t after = apr_time_now();
-    return (after - before)/(runLength*1000);
-}
 
-double DelayedLoop(LoggerPtr logger, const std::string& msg)
-{
+    //memPrint();
+    (*loop)(1000, logger);
+    //memPrint();
 
-    log4cxx_time_t before = apr_time_now();
-    int j = 0;
-    for(int i = 0; i < runLength; i++)
-        {
-                logger->info(msg, LOG4CXX_LOCATION);
-                if(j++ == burstLen)
-                {
-                        j = 0;
-                        try
-                        {
-                                apr_sleep(delay * 1000);
-                        }
-                        catch(Exception&)
-                        {
-                        }
-                }
+    long res = (*loop)(runLength, logger);
+    double average = (res * 1000.0) / runLength;
+    std::cout <<
+      "Loop completed in [" << res << "] milliseconds, or [" << average
+      << "] microseconds per log.";
 
+    //memPrint();
+  }
+
+  static void usage(const char* msg) {
+    std::cout << msg << '\n';
+    std::cout <<
+      "Usage: performance runLength configFile [char wide stream wide-stream bad-stream bad-wide-stream]\n";
+    std::cout << "\trunLength (integer) is the length of test loop.";
+    std::cout << "\tconfigFile is an XML configuration file";
+
+    exit(1);
+  }
+
+  static void memPrint() {
+  }
+
+  static void init(std::vector<LogString>& args) {
+    std::basic_istringstream<logchar> is(args[0]);
+    is >> runLength;
+    DOMConfigurator::configure(args[1]);
+    if (args.size() == 3) {
+      if(args[2] == LOG4CXX_STR("char")) {
+        loop = Loop::loopChar;
+      } else if (args[2] == LOG4CXX_STR("wide")) {
+        loop = Loop::loopWide;
+      } else if (args[2] == LOG4CXX_STR("stream")) {
+        loop = Loop::loopStream;
+      } else if (args[2] == LOG4CXX_STR("wide-stream")) {
+        loop = Loop::loopWideStream;
+      } else if (args[2] == LOG4CXX_STR("bad-stream")) {
+        loop = Loop::loopBadStream;
+      } else if (args[2] == LOG4CXX_STR("bad-wide-stream")) {
+        loop = Loop::loopBadWideStream;
+      } else {
+        usage("Unrecognized loop type.");
+      }
     }
-    double actualTime = (apr_time_now()-before)/(runLength*1000);
-    std::cout << "actual time: " << actualTime << std::endl;
-    return (actualTime - delay*DELAY_MULT);
-}
+  }
+
+  static long loopChar(long len, LoggerPtr& logger) {
+    const char* msg = "Some fix message of medium length.";
+    apr_time_t before = apr_time_now();
+    for (int i = 0; i < len; i++) {
+      LOG4CXX_DEBUG(logger, msg);
+    }
+    return (apr_time_now() - before) / 1000;
+  }
+
+  static long loopWide(long len, LoggerPtr& logger) {
+    const wchar_t* msg = L"Some fix message of medium length.";
+    apr_time_t before = apr_time_now();
+    for (int i = 0; i < len; i++) {
+      LOG4CXX_DEBUG(logger, msg);
+    }
+    return (apr_time_now() - before) / 1000;
+  }
+
+  static long loopStream(long len, LoggerPtr& logger) {
+    const char* msg = "Some fix message of medium length.";
+    apr_time_t before = apr_time_now();
+    logstream ls(logger, Level::DEBUG);
+    for (int i = 0; i < len; i++) {
+      ls << msg << LOG4CXX_ENDMSG;
+    }
+    return (apr_time_now() - before) / 1000;
+  }
+
+  static long loopWideStream(long len, LoggerPtr& logger) {
+    const wchar_t* msg = L"Some fix message of medium length.";
+    apr_time_t before = apr_time_now();
+    logstream ls(logger, Level::DEBUG);
+    for (int i = 0; i < len; i++) {
+      ls << msg << LOG4CXX_ENDMSG;
+    }
+    return (apr_time_now() - before) / 1000;
+  }
+
+  static long loopBadStream(long len, LoggerPtr& logger) {
+    const char* msg = "Some fix message of medium length.";
+    apr_time_t before = apr_time_now();
+    for (int i = 0; i < len; i++) {
+      logstream ls(logger, Level::DEBUG);
+      ls << msg << LOG4CXX_ENDMSG;
+    }
+    return (apr_time_now() - before) / 1000;
+  }
+
+  static long loopBadWideStream(long len, LoggerPtr& logger) {
+    const wchar_t* msg = L"Some fix message of medium length.";
+    apr_time_t before = apr_time_now();
+    for (int i = 0; i < len; i++) {
+      logstream ls(logger, Level::DEBUG);
+      ls << msg << LOG4CXX_ENDMSG;
+    }
+    return (apr_time_now() - before) / 1000;
+  }
+
+
+};
+
+LoggerPtr Loop::logger(Logger::getLogger("org.apache.log4j.performance.Loop"));
+int Loop::runLength = 0;
+Loop::loopFunc Loop::loop = Loop::loopChar;
+
 
 int main(int argc, const char* const argv[])
 {
         apr_app_initialize(&argc, &argv, NULL);
-        int ret = EXIT_SUCCESS;
-
-        try
-        {
-                if(argc == 3)
-                        init(argv[1], argv[2], "", "");
-                else if(argc == 5)
-                        init(argv[1], argv[2], argv[3], argv[4]);
-                else
-                        Usage(argv[0], "Wrong number of arguments.");
-
-
-                NDC::push(_T("some context"));
-
-                double delta;
-                std::string msg = _T("ABCDEGHIJKLMNOPQRSTUVWXYZabcdeghijklmnopqrstuvwxyz1234567890");
-                if(delay <= 0)
-                {
-                        delta = NoDelayLoop(logger, msg);
-                }
-                else
-                {
-                        delta = DelayedLoop(logger, msg);
-                }
-
-                std::cout << (int)delta << std::endl;
-
-                LogManager::shutdown();
+        std::vector<LogString> args(argc - 1);
+        for (int i = 1; i < argc; i++) {
+           Transcoder::decode(argv[i], args[i - 1]);
         }
-        catch(Exception&)
-        {
-                ret = EXIT_FAILURE;
-        }
-
-        apr_terminate();
-
-        return ret;
+        Loop::main(args);
 }
