477b02476b41bf597499498b30f12d4830ab99ce
Curt Arnold <carnold@apache.org>
2005-03-15 06:39:31 +0000

    LOGCXX-10: Override apr-iconv's module locating logic, utf-8 decoding

--- src/iconv_module.cpp
+++ src/iconv_module.cpp
@@ -1,3 +1,29 @@
+/*
+ * Copyright 2003,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ /*
+ *
+ * This module replaces apr-iconv's iconv_module which
+ *  uses dynamic loading of encoding modules.  This
+ *  implementation includes all the stock modules
+ *  in distinct namespaces (since otherwise they
+ *  would introduce conflicting symbols).
+ *
+ */
+
 #define ICONV_INTERNAL
 extern "C" {
   #include <apr.h>
