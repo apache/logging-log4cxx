66fb5228afc7c6208e65bc046007dd3045dbb235
Curt Arnold <carnold@apache.org>
2006-05-19 16:34:30 +0000

    LOGCXX-132: Switch from InterlockedExchangePointer back to APR atomic

--- src/objectptr.cpp
+++ src/objectptr.cpp
@@ -29,10 +29,6 @@ void ObjectPtrBase::checkNull(const int& null) {
 }
 
 void* ObjectPtrBase::exchange(volatile void** destination, void* newValue) {
-#if defined(_WIN32)
-    return InterlockedExchangePointer(destination, newValue);
-#else
    return apr_atomic_casptr(destination, newValue, (const void*) *destination);
-#endif
 }
 
