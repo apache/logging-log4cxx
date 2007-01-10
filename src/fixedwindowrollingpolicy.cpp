bce37d4805e0c0e1c076c482cb2fea2cc02578fd
Curt Arnold <carnold@apache.org>
2007-01-10 22:28:31 +0000

    LOGCXX-155: Update source headers per new ASF header policy

--- src/fixedwindowrollingpolicy.cpp
+++ src/fixedwindowrollingpolicy.cpp
@@ -1,9 +1,10 @@
 /*
- * Copyright 1999,2004 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #include <log4cxx/logstring.h>
 #include <log4cxx/rolling/fixedwindowrollingpolicy.h>
 #include <log4cxx/helpers/pool.h>
@@ -294,5 +294,6 @@ FixedWindowRollingPolicy::FileNamePatternMap::FileNamePatternMap() {
   RULES_PUT("i", IntegerPatternConverter);
   RULES_PUT("index", IntegerPatternConverter);
 }
+
 
 
