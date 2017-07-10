d659d746397beba05d871ed6840736a404d10832
Thorsten Schöning <tschoening@am-soft.de>
2017-07-10 09:36:48 +0200

    Added commits to dist-dev-repo and such.

--- releasePerform.sh
+++ releasePerform.sh
@@ -25,20 +25,43 @@
 # contents(!) of the linked dirs then. And always copying things around seems a
 # bit unnecessary as well, so I'm using a relocation of the folder for now. The
 # downside is that "mvn clean" is ignoring that dir by default...
-WD="$(pwd)/../log4cxx-next_stable"
-mvn release:perform "-DworkingDirectory=${WD}"
+WD_RELEASE="$(pwd)/../log4cxx-next_stable"
+WD_DIST_DEV="$(pwd)/../log4cxx-dist-dev"
+
+rm -rf "${WD_RELEASE}"
+mvn release:perform "-DworkingDirectory=${WD_RELEASE}"
+
+# Prepare dist/dev to get the release candidate published for a vote.
+mkdir -p "${WD_DIST_DEV}"
+pushd    "${WD_DIST_DEV}" > /dev/null
+if [ ! -d ".svn" ]
+then
+  svn co "https://dist.apache.org/repos/dist/dev/logging/log4cxx" .
+fi
+svn up
 
 # Might be a good idea to have another look at the GBG plugin for Maven in the
 # future:
 #
 # http://blog.sonatype.com/2010/01/how-to-generate-pgp-signatures-with-maven/
 # http://maven.apache.org/plugins/maven-gpg-plugin/
-pushd "${WD}"
+pushd "${WD_RELEASE}/target" > /dev/null
 for file in *.tar.gz *.zip
 do
   echo "Processing ${file}:"
 
+  gpg -ab --yes "${file}" > "${file}.asc"
   md5sum        "${file}" > "${file}.md5"
   sha512sum     "${file}" > "${file}.sha"
-  gpg -ab --yes "${file}" > "${file}.asc"
+
+  # No symlinks because those would be treated as as, no hardlinks because it
+  # should be safer for commits.
+  cp  --force   "${file}"     "${WD_DIST_DEV}"
+  cp  --force   "${file}.asc" "${WD_DIST_DEV}"
+  cp  --force   "${file}.md5" "${WD_DIST_DEV}"
+  cp  --force   "${file}.sha" "${WD_DIST_DEV}"
 done
+
+pushd "${WD_DIST_DEV}" > /dev/null
+svn add --force *.*
+svn ci  -m "Adding artifacts for new release to vote on."
