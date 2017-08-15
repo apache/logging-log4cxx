991982f917de3f16cf09b5d06445e65d540ec85c
Thorsten Schöning <tschoening@am-soft.de>
2017-08-15 16:43:21 +0200

    Changed ther script to create a new branch next_stable for releases, because releases might contain errors which need to be fixed fast, while development in master might already have continued etc. How maven release plugin handles RCs, new versions in the developoment iteration etc. seems a bit strange to me.

--- releasePrepare.sh
+++ releasePrepare.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -e
+#! /bin/sh
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -22,18 +22,44 @@
 # and quite a lot of the needed changes are possible in this script, so that is
 # preferred over manually following some docs in the wiki.
 #
+branch_starting=$(      git branch | grep "\*" | cut -d " " -f 2)
+branch_starting_is_ns=$(git branch | grep "\* next_stable")
 
-TODAY=$(date "+%Y-%m-%d")
-sed -i -r "s/date=\"XXXX-XX-XX\"/date=\"${TODAY}\"/" "src/changes/changes.xml"
+if [ -z "${branch_starting_is_ns}" ]
+then
+  git checkout "next_stable" || git checkout -b "next_stable"
+  git merge "${branch_starting}"
+fi
+
+today=$(date "+%Y-%m-%d")
+sed -i -r "1,/date=\".+?\"/ s/date=\".+?\"/date=\"${today}\"/" "src/changes/changes.xml"
 git add "src/changes/changes.xml"
-git diff-index --quiet HEAD || git commit -m "Set release date to today."
+if git diff-index --quiet HEAD
+then
+  git commit -m "Set release date to today."
+  commit_changes=$(git log --max-count=1 | grep "commit" | cut -d " " -f 2)
+  git checkout "${branch_starting}"
+  git merge    "${commit_changes}"
+  git checkout "next_stable"
+fi
 
-mvn clean
-mvn release:prepare -Dresume=false
+mvn clean                          || exit 1
+mvn release:prepare -Dresume=false || exit 1
+
+if [ -z "${branch_starting_is_ns}" ]
+then
+  git checkout "${branch_starting}"
+  new_release_cycle=$(grep 'date="XXXX-XX-XX"' "src/changes/changes.xml")
+  if [ -n "${new_release_cycle}" ]
+  then
+    git checkout "next_stable"
+    exit 0
+  fi
+fi
 
 # Propagate new version into some additional files:
-NEW_DEV_VER_SHORT=$(grep -E "^project.dev.log4cxx" "release.properties" | cut -d = -f 2 | cut -d - -f 1)
-NEW_RELEASE=$(cat <<-"END"
+new_dev_ver_short=$(grep -E "^project.dev.log4cxx" "release.properties" | cut -d "=" -f 2 | cut -d - -f 1)
+new_release=$(cat <<-"END"
 	<body>\n\
 		<release	version="VER_NEEDED"\n\
 					date="XXXX-XX-XX"\n\
@@ -41,11 +67,15 @@ NEW_RELEASE=$(cat <<-"END"
 		<\/release>\n
 END
 )
-NEW_RELEASE="${NEW_RELEASE/VER_NEEDED/${NEW_DEV_VER_SHORT}}"
+new_release="${new_release/VER_NEEDED/${new_dev_ver_short}}"
 
-sed -i -r "s/AC_INIT\(\[log4cxx\], \[.+?\]\)/AC_INIT([log4cxx], [${NEW_DEV_VER_SHORT}])/" "configure.ac"
-sed -i -r "s/<body>/${NEW_RELEASE}/" "src/changes/changes.xml"
+sed -i -r "s/AC_INIT\(\[log4cxx\], \[.+?\]\)/AC_INIT([log4cxx], [${new_dev_ver_short}])/" "configure.ac"
+sed -i -r "s/<body>/${new_release}/" "src/changes/changes.xml"
 
 git add "configure.ac"
 git add "src/changes/changes.xml"
-git diff-index --quiet HEAD || git commit -m "Prepare for next development iteration: ${NEW_DEV_VER_SHORT}"
+if git diff-index --quiet HEAD
+then
+  git commit -m "Prepare for next development iteration: ${new_dev_ver_short}"
+fi
+git checkout "next_stable"
