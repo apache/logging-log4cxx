b9ea65496003b9cb8f29ddc76e0c8f21e18ae862
Patrick Mills <patrick.mills@cerberusftp.com>
2020-06-02 19:29:37 -0400

    Implement mapfilter Build unit test for mapfilter

--- src/main/include/log4cxx/filter/mapfilter.h
+++ src/main/include/log4cxx/filter/mapfilter.h
@@ -19,26 +19,87 @@
 
 #include <log4cxx/spi/filter.h>
 
+#if defined(_MSC_VER)
+	#pragma warning ( push )
+	#pragma warning ( disable: 4251 )
+#endif
+
 namespace log4cxx
 {
 namespace filter
 {
+/**
+ * A Filter that operates on a Map.
+ */
 
+ using keyvallist = std::map<LogString, LogString>;
 
 class LOG4CXX_EXPORT MapFilter: public log4cxx::spi::Filter
 {
+	private:
+		bool acceptOnMatch{true};
+		bool mustMatchAll{false};	// true = AND; false = OR
+		keyvallist mapKeyValList;
+
 	public:
 		DECLARE_LOG4CXX_OBJECT(MapFilter)
 		BEGIN_LOG4CXX_CAST_MAP()
-		LOG4CXX_CAST_ENTRY(log4cxx::spi::Filter)
+		LOG4CXX_CAST_ENTRY(MapFilter)
+		LOG4CXX_CAST_ENTRY_CHAIN(log4cxx::spi::Filter)
 		END_LOG4CXX_CAST_MAP()
 
-		MapFilter();
+		MapFilter(){}
 
+		/**
+		Set options
+		*/
+		virtual void setOption(const LogString& option,
+			const LogString& value);
 
+		inline void setKeyValue(const LogString& strKey, const LogString& strValue)
+		{
+			this->mapKeyValList[strKey] = strValue;
+		}
+
+		inline const LogString& getValue(const LogString& strKey) const
+		{
+			static const LogString empty{};
+			auto it = mapKeyValList.find(strKey);
+			return (it != mapKeyValList.end() ? it->second : empty);
+		}
+
+		inline void setAcceptOnMatch(bool acceptOnMatch1)
+		{
+			this->acceptOnMatch = acceptOnMatch1;
+		}
+
+		inline bool getAcceptOnMatch() const
+		{
+			return acceptOnMatch;
+		}
+
+		inline bool getMustMatchAll() const
+		{
+			return mustMatchAll;
+		}
+
+		inline void setMustMatchAll(bool mustMatchAll1)
+		{
+			this->mustMatchAll = mustMatchAll1;
+		}
+
+		/**
+		Returns {@link log4cxx::spi::Filter#NEUTRAL NEUTRAL}
+		is there is no string match.
+		*/
 		FilterDecision decide(const spi::LoggingEventPtr& event) const;
+}; // class MapFilter
+LOG4CXX_PTR_DEF(MapFilter);
+}  // namespace filter
+} // namespace log4cxx
 
-};
-}
-}
+#if defined(_MSC_VER)
+	#pragma warning (pop)
 #endif
+
+#endif // _LOG4CXX_FILTER_MAPFILTER_H
