9f34e9ee541136d98903d6088776a64533dd820d
Stephen Webb <stephen.webb@sabreautonomous.com.au>
2022-12-31 11:52:35 +1100

    Prevent compilation error using C++11 standard

--- src/cmake/boost-fallback/makeunique.h.cmake
+++ src/cmake/boost-fallback/makeunique.h.cmake
@@ -1,16 +1,18 @@
 #ifndef LOG4CXX_MAKE_UNIQUE_H
 #define LOG4CXX_MAKE_UNIQUE_H
+#include <memory>
 
 #cmakedefine01 STD_MAKE_UNIQUE_FOUND
 
 #if !STD_MAKE_UNIQUE_FOUND
-namespace std{
-template<typename T, typename ...Args>
-std::unique_ptr<T> make_unique( Args&& ...args )
+namespace std
 {
-    return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+	template <typename T, typename ...Args>
+unique_ptr<T> make_unique( Args&& ...args )
+{
+	return unique_ptr<T>( new T( forward<Args>(args)... ) );
 }
-}
+} // namespace std
 #endif
 
 #endif /* LOG4CXX_MAKE_UNIQUE_H */
