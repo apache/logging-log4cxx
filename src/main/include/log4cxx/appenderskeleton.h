faeeb2c9521fd583c0bc6d1a0b2543d287a9e8ae
Stephen Webb <swebb2066@gmail.com>
2023-04-29 22:01:14 +1000

    Fully remove false statement

--- src/main/include/log4cxx/appenderskeleton.h
+++ src/main/include/log4cxx/appenderskeleton.h
@@ -145,7 +145,7 @@ class LOG4CXX_EXPORT AppenderSkeleton :
 
 		/**
 		Set the layout for this appender. Note that some appenders have
-		their own (fixed) layouts or do not use one. For example, the
+		their own (fixed) layouts or do not use one.
 		*/
 		void setLayout(const LayoutPtr layout1) override;
 
