3a85f5cdb365488642e05a8ec3025390f2774519
Robert Middleton <rm5248@users.noreply.github.com>
2023-07-11 23:02:23 -0400

    Added a utility method to hexdump bytes (#228)
    Add utility method to hexdump bytes.
    
    Fix #214

---
+++ src/main/cpp/hexdump.cpp
@@ -0,0 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <log4cxx/hexdump.h>
+#include <log4cxx/log4cxx.h>
+#include <sstream>
+#include <ios>
+#include <iomanip>
+
+using namespace log4cxx;
+
+typedef std::basic_stringstream<logchar> LogStream;
+
+LogString log4cxx::hexdump(const void* bytes, uint32_t len, HexdumpFlags flags){
+	LogString ret;
+	const uint8_t* bytes_u8 = static_cast<const uint8_t*>(bytes);
+	LogStream sstream;
+#if LOG4CXX_LOGCHAR_IS_WCHAR
+	const wchar_t fill_char = L'0';
+	const wchar_t space_fill_char = L' ';
+#else
+	const char fill_char = '0';
+	const char space_fill_char = ' ';
+#endif
+
+	if(flags & HexdumpFlags::AddStartingNewline){
+		sstream << LOG4CXX_EOL;
+	}
+
+	for(uint32_t offset = 0; offset < len; offset += 16){
+		if(offset != 0){
+			sstream << LOG4CXX_EOL;
+		}
+
+		// Print out the offset
+		sstream << std::hex << std::setw(8) << std::setfill(fill_char) << offset << std::resetiosflags(std::ios_base::fmtflags(0));
+
+		sstream << std::setw(0) << LOG4CXX_STR("  ");
+
+		// Print out the first 8 bytes
+		for(int byte = 0; byte < 8; byte++){
+			if(offset + byte >= len){
+				sstream << LOG4CXX_STR("  ");
+				if(byte != 8){
+					sstream << LOG4CXX_STR(" ");
+				}
+				continue;
+			}
+
+			sstream << std::hex << std::setw(2) << std::setfill(fill_char) << static_cast<int>(bytes_u8[offset + byte]) << std::resetiosflags(std::ios_base::fmtflags(0));
+			sstream << std::setfill(space_fill_char);
+			if(byte != 8){
+				sstream << LOG4CXX_STR(" ");
+			}
+		}
+
+		sstream << LOG4CXX_STR(" ");
+
+		// Print out the last 8 bytes
+		for(int byte = 8; byte < 16; byte++){
+			if(offset + byte >= len){
+				sstream << LOG4CXX_STR("  ");
+				if(byte != 15){
+					sstream << LOG4CXX_STR(" ");
+				}
+				continue;
+			}
+
+			sstream << std::hex << std::setw(2) << std::setfill(fill_char) << static_cast<int>(bytes_u8[offset + byte]) << std::resetiosflags(std::ios_base::fmtflags(0));
+			if(byte != 15){
+				sstream << LOG4CXX_STR(" ");
+			}
+		}
+
+		// Print out the ASCII text
+		sstream << LOG4CXX_STR("  |");
+		for(int byte = 0; byte < 16; byte++){
+			if(offset + byte >= len){
+				break;
+			}
+			if(std::isprint(bytes_u8[offset + byte])){
+				logchar to_append = bytes_u8[offset + byte];
+				sstream << to_append;
+			}else{
+				sstream << LOG4CXX_STR(".");
+			}
+		}
+		sstream << LOG4CXX_STR("|");
+	}
+
+	if(flags & HexdumpFlags::AddEndingNewline){
+		sstream << LOG4CXX_EOL;
+	}
+
+	return sstream.str();
+}
---
+++ src/test/cpp/hexdumptestcase.cpp
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "logunit.h"
+#include <log4cxx/hexdump.h>
+#include <iostream>
+
+using namespace log4cxx;
+
+LOGUNIT_CLASS(HexdumpTestCase)
+{
+	LOGUNIT_TEST_SUITE(HexdumpTestCase);
+	LOGUNIT_TEST(test1);
+	LOGUNIT_TEST(test2);
+	LOGUNIT_TEST(test_newline);
+	LOGUNIT_TEST(test_newline2);
+	LOGUNIT_TEST_SUITE_END();
+
+public:
+	void setUp()
+	{
+	}
+
+	void tearDown()
+	{
+	}
+
+	void test1()
+	{
+		unsigned char test1_str[] = {
+		  0x74, 0x65, 0x73, 0x74, 0x31
+		};
+
+		LogString expectedOutput =
+				LOG4CXX_STR("00000000  74 65 73 74 31                                    |test1|");
+
+		LogString dumped = log4cxx::hexdump(test1_str, sizeof(test1_str));
+		LOGUNIT_ASSERT_EQUAL(expectedOutput, dumped);
+	}
+
+	void test2()
+	{
+		unsigned char quick_brown_fox[] = {
+		  0x54, 0x68, 0x65, 0x20, 0x71, 0x75, 0x69, 0x63, 0x6b, 0x20, 0x62, 0x72,
+		  0x6f, 0x77, 0x6e, 0x20, 0x66, 0x6f, 0x78, 0x20, 0x6a, 0x75, 0x6d, 0x70,
+		  0x73, 0x20, 0x6f, 0x76, 0x65, 0x72, 0x20, 0x74, 0x68, 0x65, 0x20, 0x6c,
+		  0x61, 0x7a, 0x79, 0x20, 0x64, 0x6f, 0x67
+		};
+		LogString expectedOutput =
+				LOG4CXX_STR("00000000  54 68 65 20 71 75 69 63  6b 20 62 72 6f 77 6e 20  |The quick brown |")
+				LOG4CXX_EOL
+				LOG4CXX_STR("00000010  66 6f 78 20 6a 75 6d 70  73 20 6f 76 65 72 20 74  |fox jumps over t|")
+				LOG4CXX_EOL
+				LOG4CXX_STR("00000020  68 65 20 6c 61 7a 79 20  64 6f 67                 |he lazy dog|");
+
+		// Hexdump up until the NULL char
+		LogString dumped = log4cxx::hexdump(quick_brown_fox, sizeof(quick_brown_fox));
+		LOGUNIT_ASSERT_EQUAL(expectedOutput, dumped);
+	}
+
+	void test_newline()
+	{
+		unsigned char test1_str[] = {
+		  0x74, 0x65, 0x73, 0x74, 0x31
+		};
+		LogString expectedOutput =
+				LOG4CXX_EOL
+				LOG4CXX_STR("00000000  74 65 73 74 31                                    |test1|")
+				LOG4CXX_EOL;
+
+		LogString dumped = log4cxx::hexdump(test1_str, sizeof(test1_str), HexdumpFlags::AddNewline);
+		LOGUNIT_ASSERT_EQUAL(expectedOutput, dumped);
+	}
+
+	void test_newline2()
+	{
+		unsigned char test1_str[] = {
+		  0x74, 0x65, 0x73, 0x74, 0x31
+		};
+		LogString expectedOutput =
+				LOG4CXX_EOL
+				LOG4CXX_STR("00000000  74 65 73 74 31                                    |test1|")
+				LOG4CXX_EOL;
+
+		LogString dumped = log4cxx::hexdump(test1_str, sizeof(test1_str), HexdumpFlags::AddStartingNewline | HexdumpFlags::AddEndingNewline);
+		LOGUNIT_ASSERT_EQUAL(expectedOutput, dumped);
+	}
+
+};
+
+
+LOGUNIT_TEST_SUITE_REGISTRATION(HexdumpTestCase);
