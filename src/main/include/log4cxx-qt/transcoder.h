4cd04d5b47de95508c1558e34304ecdb27d422ef
Stephen Webb <stephen.webb@ieee.org>
2023-08-19 12:19:30 +1000

    Provide descriptions of character encoding/decoding macros (#261)

--- src/main/include/log4cxx-qt/transcoder.h
+++ src/main/include/log4cxx-qt/transcoder.h
@@ -21,25 +21,79 @@
 #include <QString>
 
 #if LOG4CXX_LOGCHAR_IS_UTF8
+/** Create a log4cxx::LogString equivalent of \c src.
+
+	Defines a log4cxx::LogString variable \c var
+	initialized with characters
+	equivalent to the QString \c src contents.
+
+	@param var The name of the new log4cxx::LogString variable.
+	@param src The QString variable.
+*/
 #define LOG4CXX_DECODE_QSTRING(var, src) \
 	log4cxx::LogString var = (src).toStdString()
 
+/** Create a QString equivalent of \c src.
+
+	Defines a QString variable \c var
+	initialized with characters
+	equivalent to the log4cxx::LogString \c src contents.
+
+	@param var The name of the new QString variable.
+	@param src The log4cxx::LogString variable.
+*/
 #define LOG4CXX_ENCODE_QSTRING(var, src) \
 	QString var = QString::fromStdString(src)
 #endif // LOG4CXX_LOGCHAR_IS_UTF8
 
 #if LOG4CXX_LOGCHAR_IS_WCHAR
+/** Create a log4cxx::LogString equivalent of \c src.
+
+	Defines a log4cxx::LogString variable \c var
+	initialized with characters
+	equivalent to the QString \c src contents.
+
+	@param var The name of the new log4cxx::LogString variable.
+	@param src The QString variable.
+*/
 #define LOG4CXX_DECODE_QSTRING(var, src) \
 	log4cxx::LogString var = (src).toStdWString()
 
+/** Create a QString equivalent of \c src.
+
+	Defines a QString variable \c var
+	initialized with characters
+	equivalent to the log4cxx::LogString \c src contents.
+
+	@param var The name of the new QString variable.
+	@param src The log4cxx::LogString variable.
+*/
 #define LOG4CXX_ENCODE_QSTRING(var, src) \
 	QString var = QString::fromStdWString(src)
 #endif // LOG4CXX_LOGCHAR_IS_WCHAR
 
 #if LOG4CXX_LOGCHAR_IS_UNICHAR
+/** Create a log4cxx::LogString equivalent of \c src.
+
+	Defines a log4cxx::LogString variable \c var
+	initialized with characters
+	equivalent to the QString \c src contents.
+
+	@param var The name of the new log4cxx::LogString variable.
+	@param src The QString variable.
+*/
 #define LOG4CXX_DECODE_QSTRING(var, src) \
 	log4cxx::LogString var = (src).utf16()
 
+/** Create a QString equivalent of \c src.
+
+	Defines a QString variable \c var
+	initialized with characters
+	equivalent to the log4cxx::LogString \c src contents.
+
+	@param var The name of the new QString variable.
+	@param src The log4cxx::LogString variable.
+*/
 #define LOG4CXX_ENCODE_QSTRING(var, src) \
 	QString var = QString::fromUtf16((char16_t*)src.c_str())
 #endif // LOG4CXX_LOGCHAR_IS_UNICHAR
