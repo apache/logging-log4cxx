ce3c7178439996411cc858639dd6ea04aac20942
Stephen Webb <swebb2066@gmail.com>
2024-03-05 13:27:31 +1100

    Benchmark should include the cost of buffered I/O

--- src/test/cpp/benchmark/benchmark.cpp
+++ src/test/cpp/benchmark/benchmark.cpp
@@ -297,7 +297,7 @@ BENCHMARK_DEFINE_F(benchmarker, fileIntPlusFloatValueMessageBuffer)(benchmark::S
 	for (auto _ : state)
 	{
 		auto f = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-		LOG4CXX_INFO( m_logger, "Hello: message number " << ++x
+		LOG4CXX_INFO( m_fileLogger, "Hello: message number " << ++x
 			<< " pseudo-random float " << std::setprecision(3) << std::fixed << f);
 	}
 }
