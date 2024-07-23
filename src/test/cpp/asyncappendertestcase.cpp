d29d30a6577e6da8e1c460f037b259e3cb9a5228
Stephen Webb <swebb2066@gmail.com>
2024-07-23 16:09:27 +1000

    Prevent the occasional (timing related) test case failure

--- src/test/cpp/asyncappendertestcase.cpp
+++ src/test/cpp/asyncappendertestcase.cpp
@@ -309,7 +309,7 @@ class AsyncAppenderTestCase : public AppenderSkeletonTestCase
 			asyncAppender->setErrorHandler(errorHandler);
 
 			LOG4CXX_INFO(root, "Message");
-			std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
+			std::this_thread::sleep_for( std::chrono::milliseconds( 30 ) );
 			LOGUNIT_ASSERT(errorHandler->errorReported());
 			LOG4CXX_INFO(root, "Message");
 			auto& v = vectorAppender->getVector();
