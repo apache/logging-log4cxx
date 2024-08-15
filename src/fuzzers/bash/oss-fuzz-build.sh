b906c2bf4c696a90ee98d472cd542993f51d6b65
Volkan Yazıcı <volkan@yazi.ci>
2024-08-15 11:31:18 +0200

    Minor improvements to `oss-fuzz-build.sh`

--- src/fuzzers/bash/oss-fuzz-build.sh
+++ src/fuzzers/bash/oss-fuzz-build.sh
@@ -40,8 +40,13 @@ cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && cd ../../..
 
 # Build the project
 mkdir -p build && cd $_
-cmake -DBUILD_SHARED_LIBS=OFF -DBUILD_FUZZERS=ON ..
-make -j
+cmake \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_TESTING=OFF \
+  -DBUILD_EXAMPLES=OFF \
+  -DBUILD_FUZZERS=ON \
+  ..
+cmake --build .
 
 # Copy executables & resources
 find src/fuzzers/cpp -maxdepth 1 -executable -type f -exec cp -v {} "$outputDir/" \;
