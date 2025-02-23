c866dfc1906d151800cc1fcb990a3d989e3d62cc
Stephen Webb <stephen.webb@ieee.org>
2025-02-23 13:28:17 +1100

    Prevent timeout fault running PatternConverterFuzzer (#481)

--- src/fuzzers/cpp/PatternConverterFuzzer.cpp
+++ src/fuzzers/cpp/PatternConverterFuzzer.cpp
@@ -15,14 +15,11 @@
  * limitations under the License.
  */
 
-#include "stdint.h"
-#include <log4cxx/nt/nteventlogappender.h>
-#include <log4cxx/logstring.h>
-#include <log4cxx/helpers/stringhelper.h>
 #include <fuzzer/FuzzedDataProvider.h>
-#include <log4cxx/pattern/patternconverter.h>
+#include <log4cxx/spi/loggingevent.h>
 #include <log4cxx/helpers/transcoder.h>
 
+#include <log4cxx/pattern/patternconverter.h>
 #include <log4cxx/pattern/loggerpatternconverter.h>
 #include <log4cxx/pattern/literalpatternconverter.h>
 #include <log4cxx/pattern/classnamepatternconverter.h>
@@ -43,29 +40,25 @@
 #include <log4cxx/pattern/throwableinformationpatternconverter.h>
 #include <log4cxx/pattern/threadusernamepatternconverter.h>
 
+namespace
+{
+	const int MaximumOptionByteCount = 1000;
+	const int MaximumNameByteCount = 100;
+}
+
 using namespace log4cxx;
 using namespace log4cxx::helpers;
 using namespace log4cxx::spi;
 using namespace log4cxx::pattern;
 
-#if LOG4CXX_WCHAR_T_API || LOG4CXX_LOGCHAR_IS_WCHAR_T
-wchar_t* wencode(const std::string& src, Pool& p)
-{
-        wchar_t* dst = (wchar_t*) p.palloc((src.length() + 1) * sizeof(wchar_t));
-        dst[src.length()] = 0;
-        std::memcpy(dst, src.data(), src.length() * sizeof(wchar_t));
-        return dst;
-}
-#endif
-
 // Creates options from the FuzzedDataProvider
-std::vector<log4cxx::LogString> createOptions(FuzzedDataProvider* fdp) {
-	std::string opt1Str = fdp->ConsumeRandomLengthString();
-	std::string opt2Str = fdp->ConsumeRandomLengthString();
-	std::string opt3Str = fdp->ConsumeRandomLengthString();
-	std::string opt4Str = fdp->ConsumeRandomLengthString();
-	std::string opt5Str = fdp->ConsumeRandomLengthString();
-	
+auto createOptions(FuzzedDataProvider* fdp) {
+	auto opt1Str = fdp->ConsumeRandomLengthString(MaximumOptionByteCount);
+	auto opt2Str = fdp->ConsumeRandomLengthString(MaximumOptionByteCount);
+	auto opt3Str = fdp->ConsumeRandomLengthString(MaximumOptionByteCount);
+	auto opt4Str = fdp->ConsumeRandomLengthString(MaximumOptionByteCount);
+	auto opt5Str = fdp->ConsumeRandomLengthString(MaximumOptionByteCount);
+
 	LogString opt1, opt2, opt3, opt4, opt5;
 
 	Transcoder::decode(opt1Str, opt1);
@@ -74,121 +67,101 @@ std::vector<log4cxx::LogString> createOptions(FuzzedDataProvider* fdp) {
 	Transcoder::decode(opt4Str, opt4);
 	Transcoder::decode(opt5Str, opt5);
 
-	std::vector<log4cxx::LogString> options =
-        { opt1
-        , opt2
-        , opt3
-        , opt4
-        , opt5
-        };
-       return options;
+	return std::vector<LogString>
+		{ opt1
+		, opt2
+		, opt3
+		, opt4
+		, opt5
+		};
 }
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 	// Create a FuzzedDataProvider which we will use to
 	// create strings from "data".
 	FuzzedDataProvider fdp(data, size);
-	
-	std::string loggerStr = fdp.ConsumeRandomLengthString();
-	std::string contentStr = fdp.ConsumeRandomLengthString();
-	
+
+	auto loggerStr = fdp.ConsumeRandomLengthString();
+	auto contentStr = fdp.ConsumeRandomLengthString();
+
 	LogString logger, content;
 	Transcoder::decode(loggerStr, logger);
 	Transcoder::decode(contentStr, content);
 
 	// Create the event
-	LevelPtr level = Level::getInfo();
+	auto level = Level::getInfo();
 
 	Pool p;
 
-	log4cxx::spi::LoggingEventPtr event = log4cxx::spi::LoggingEventPtr(
-		new log4cxx::spi::LoggingEvent(
-			logger, level, content, LOG4CXX_LOCATION));
+	auto event = std::make_shared<LoggingEvent>(
+			logger, level, content, LOG4CXX_LOCATION);
 	// Select a converter and invoke it.
 	switch(fdp.ConsumeIntegralInRange<int>(0, 14)) {
 		case 0: {
-			std::string str1 = fdp.ConsumeRandomLengthString();
-			std::string str2 = fdp.ConsumeRandomLengthString();
-			LogString a1, a2;
-			Transcoder::decode(str1, a1);
-			Transcoder::decode(str2, a2);
+			auto nameStr = fdp.ConsumeRandomLengthString(MaximumNameByteCount);
+			auto opionStr = fdp.ConsumeRandomLengthString(MaximumOptionByteCount);
+			LogString name, option;
+			Transcoder::decode(nameStr, name);
+			Transcoder::decode(opionStr, option);
 
-			PropertiesPatternConverter* converter = new PropertiesPatternConverter(a1, a2);
-			converter->format(event, logger, p);
-			delete converter;
+			PropertiesPatternConverter(name, option).format(event, logger, p);
+			break;
 		}
 		case 1: {
-			std::vector<log4cxx::LogString> options = createOptions(&fdp);
-			LoggerPatternConverter* converter = new LoggerPatternConverter(options);
-			converter->format(event, logger, p);
-			delete converter;
+			LoggerPatternConverter(createOptions(&fdp)).format(event, logger, p);
+			break;
 		}
 		case 2: {
-			std::vector<log4cxx::LogString> options = createOptions(&fdp);
-			ClassNamePatternConverter* converter = new ClassNamePatternConverter(options);
-			converter->format(event, logger, p);
-			delete converter;
+			ClassNamePatternConverter(createOptions(&fdp)).format(event, logger, p);
+			break;
 		}
 		case 3: {
-			std::vector<log4cxx::LogString> options = createOptions(&fdp);
-			DatePatternConverter* converter = new DatePatternConverter(options);
-			converter->format(event, logger, p);
-			delete converter;
+			DatePatternConverter(createOptions(&fdp)).format(event, logger, p);
+			break;
 		}
 		case 4: {
-			FullLocationPatternConverter* converter = new FullLocationPatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			FullLocationPatternConverter().format(event, logger, p);
+			break;
 		}
 		case 5: {
-			LineLocationPatternConverter* converter = new LineLocationPatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			LineLocationPatternConverter().format(event, logger, p);
+			break;
 		}
 		case 6: {
-			MessagePatternConverter* converter = new MessagePatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			MessagePatternConverter().format(event, logger, p);
+			break;
 		}
 		case 7: {
-			LineSeparatorPatternConverter* converter = new LineSeparatorPatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			LineSeparatorPatternConverter().format(event, logger, p);
+			break;
 		}
 		case 8: {
-			MethodLocationPatternConverter* converter = new MethodLocationPatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			MethodLocationPatternConverter().format(event, logger, p);
+			break;
 		}
 		case 9: {
-			LevelPatternConverter* converter = new LevelPatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			LevelPatternConverter().format(event, logger, p);
 		}
 		case 10: {
-			RelativeTimePatternConverter* converter = new RelativeTimePatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			RelativeTimePatternConverter().format(event, logger, p);
+			break;
 		}
 		case 11: {
-			ThreadPatternConverter* converter = new ThreadPatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			ThreadPatternConverter().format(event, logger, p);
+			break;
 		}
 		case 12: {
-			ThreadUsernamePatternConverter* converter = new ThreadUsernamePatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			ThreadUsernamePatternConverter().format(event, logger, p);
+			break;
 		}
 		case 13: {
-			NDCPatternConverter* converter = new NDCPatternConverter();
-			converter->format(event, logger, p);
-			delete converter;
+			NDCPatternConverter().format(event, logger, p);
+			break;
 		}
 		case 14: {
-			ThrowableInformationPatternConverter* converter = new ThrowableInformationPatternConverter(fdp.ConsumeBool());
-			converter->format(event, logger, p);
-			delete converter;
+			ThrowableInformationPatternConverter(fdp.ConsumeBool()).format(event, logger, p);
+			break;
 		}
 	}
-  	return 0;
+	return 0;
 }
