f3b87c084b671465043c973fab20a15ac409a6fe
Stephen Webb <swebb2066@gmail.com>
2025-02-23 12:33:04 +1100

    Remove unused headers

--- src/fuzzers/cpp/PatternConverterFuzzer.cpp
+++ src/fuzzers/cpp/PatternConverterFuzzer.cpp
@@ -15,13 +15,11 @@
  * limitations under the License.
  */
 
-#include <log4cxx/nt/nteventlogappender.h>
-#include <log4cxx/logstring.h>
-#include <log4cxx/helpers/stringhelper.h>
 #include <fuzzer/FuzzedDataProvider.h>
-#include <log4cxx/pattern/patternconverter.h>
+#include <log4cxx/spi/loggingevent.h>
 #include <log4cxx/helpers/transcoder.h>
 
+#include <log4cxx/pattern/patternconverter.h>
 #include <log4cxx/pattern/loggerpatternconverter.h>
 #include <log4cxx/pattern/literalpatternconverter.h>
 #include <log4cxx/pattern/classnamepatternconverter.h>
