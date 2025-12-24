a55e63a33d4f711f5e9e6da9a7cb8383e11c1a85
Stephen Webb <swebb2066@gmail.com>
2025-12-24 14:26:33 +1100

    Simplify Qt configuration example

--- src/examples/cpp/com/foo/config-qt.cpp
+++ src/examples/cpp/com/foo/config-qt.cpp
@@ -25,10 +25,11 @@
 
 namespace com { namespace foo {
 
+using namespace log4cxx;
+
 // Provide the name of the configuration file to Log4cxx.
 // Reload the configuration on a QFileSystemWatcher::fileChanged event.
 void ConfigureLogging() {
-	using namespace log4cxx;
 	static struct log4cxx_initializer {
 		log4cxx_initializer() {
 			qInstallMessageHandler(qt::messageHandler);
@@ -62,7 +63,6 @@ void ConfigureLogging() {
 
 // Retrieve the \c name logger pointer.
 auto getLogger(const QString& name) -> LoggerPtr {
-	using namespace log4cxx;
 	return name.isEmpty()
 		? LogManager::getRootLogger()
 		: LogManager::getLogger(name.toStdString());
@@ -70,7 +70,6 @@ auto getLogger(const QString& name) -> LoggerPtr {
 
 // Retrieve the \c name logger pointer.
 auto getLogger(const char* name) -> LoggerPtr {
-	using namespace log4cxx;
 	return name
 		? LogManager::getLogger(name)
 		: LogManager::getRootLogger();
