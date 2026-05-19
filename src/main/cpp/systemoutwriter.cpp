fa32225e61340fb29ca29c6ce5f03af79dfc356e
Stephen Webb <stephen.webb@ieee.org>
2026-05-19 12:12:51 +1000

    Clean up includes in systemoutwriter.cpp
    Removed unused includes and preprocessor directives.

--- src/main/cpp/systemoutwriter.cpp
+++ src/main/cpp/systemoutwriter.cpp
@@ -15,14 +15,7 @@
  * limitations under the License.
  */
 
-#include <log4cxx/logstring.h>
 #include <log4cxx/helpers/systemoutwriter.h>
-#include <log4cxx/helpers/transcoder.h>
-#include <stdio.h>
-#if !defined(LOG4CXX)
-	#define LOG4CXX 1
-#endif
-#include <log4cxx/private/log4cxx_private.h>
 #include <log4cxx/private/consolewriter_priv.h>
 
 using namespace LOG4CXX_NS;
