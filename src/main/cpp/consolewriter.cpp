b982e3efcd2aa4e10282d0d983e53523ec3737d9
Stephen Webb <stephen.webb@ieee.org>
2026-05-19 11:58:19 +1000

    Use of log4cx_private.h must be doubly confirmed

--- src/main/cpp/consolewriter.cpp
+++ src/main/cpp/consolewriter.cpp
@@ -17,6 +17,9 @@
 
 #include <log4cxx/private/consolewriter_priv.h>
 #include <log4cxx/helpers/transcoder.h>
+#if !defined(LOG4CXX)
+	#define LOG4CXX 1
+#endif
 #include <log4cxx/private/log4cxx_private.h>
 
 using namespace LOG4CXX_NS;
