d17cd491bc365365790b1d08bcd6493ace93773c
Stephen Webb <stephen.webb@ieee.org>
2026-05-26 11:16:31 +1000

    Add documention for the string processing methods (#689)

--- src/main/include/log4cxx/helpers/stringhelper.h
+++ src/main/include/log4cxx/helpers/stringhelper.h
@@ -35,16 +35,22 @@ String manipulation routines
 class LOG4CXX_EXPORT StringHelper
 {
 	public:
+		/// A copy of \c s without any leading or trailing space characters
 		static LogString trim(const LogString& s);
-		static bool startsWith(const LogString& s, const LogString& suffix);
+		/// Does \c s begin with the characters of \c prefix?
+		static bool startsWith(const LogString& s, const LogString& prefix);
+		/// Does \c s end with the characters of \c suffix?
 		static bool endsWith(const LogString& s, const LogString& suffix);
-		static bool equalsIgnoreCase(const LogString& s1,
-			const logchar* upper, const logchar* lower);
-		static bool equalsIgnoreCase(const LogString& s1,
-			const LogString& upper, const LogString& lower);
+		/// Is each character in \c s identical to the character at the corresponding position in either \c upper or \c lower?
+		static bool equalsIgnoreCase(const LogString& s, const logchar* upper, const logchar* lower);
+		/// Is each character in \c s identical to the character at the corresponding position in either \c upper or \c lower?
+		static bool equalsIgnoreCase(const LogString& s, const LogString& upper, const LogString& lower);
 
-
+		/// The numeric value at the start of \c s.
+		/// See <a href=https://en.cppreference.com/cpp/string/basic_string/stol>std::stoi</a> for more details.
 		static int toInt(const LogString& s);
+		/// The numeric value at the start of \c s.
+		/// See <a href=https://en.cppreference.com/cpp/string/basic_string/stol>std::stoll</a> for more details.
 		static int64_t toInt64(const LogString& s);
 
 #if LOG4CXX_ABI_VERSION <= 15
@@ -58,13 +64,21 @@ class LOG4CXX_EXPORT StringHelper
 		[[ deprecated( "Pool is no longer required" ) ]]
 		static void toString(size_t i, Pool& pool, LogString& dst);
 #endif
+		/// Append a textual version of \c i to \c dst.
 		static void toString(int i, LogString& dst);
+		/// Append a textual version of \c i to \c dst.
 		static void toString(int64_t i, LogString& dst);
+		/// Append a textual version of \c i to \c dst.
 		static void toString(size_t i, LogString& dst);
+		/// Using \c val, append either "true" or "false" to \c dst.
 		static void toString(bool val, LogString& dst);
 
+		/// A copy of \c s with any character in [A-Z]
+		/// replaced by the corresponding character in [a-z]
 		static LogString toLowerCase(const LogString& s);
 
+		/// A copy of \c pattern with any 3-character '{', [0-9], '}', substring
+		/// replaced by the corresponding value from \c params
 		static LogString format(const LogString& pattern, const std::vector<LogString>& params);
 };
 }
