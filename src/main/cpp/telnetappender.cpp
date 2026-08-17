e87ecbf1298c61bca94f93a4f258b8446089d73f
Stephen Webb <swebb2066@gmail.com>
2026-08-17 17:49:46 +1000

    Increase the TelnetAppender's encoding buffer size

--- src/main/cpp/telnetappender.cpp
+++ src/main/cpp/telnetappender.cpp
@@ -255,7 +255,7 @@ void TelnetAppender::write(ByteBuffer& buf)
 
 void TelnetAppender::writeStatus(const SocketPtr& socket, const LogString& msg, Pool& p)
 {
-	size_t bytesSize = msg.size() * 2;
+	size_t bytesSize = msg.size() * 2 + 10;
 	char* bytes = p.pstralloc(bytesSize);
 
 	LogString::const_iterator msgIter(msg.begin());
@@ -263,7 +263,7 @@ void TelnetAppender::writeStatus(const SocketPtr& socket, const LogString& msg, 
 
 	while (msgIter != msg.end())
 	{
-		_priv->encoder->encode(msg, msgIter, buf);
+		CharsetEncoder::encode(_priv->encoder, msg, msgIter, buf);
 		buf.flip();
 		socket->write(buf);
 		buf.clear();
@@ -282,7 +282,7 @@ void TelnetAppender::append( LOG4CXX_APPEND_FORMAL_PARAMETERS )
 		else
 			msg = event->getRenderedMessage();
 		msg.append(LOG4CXX_STR("\r\n"));
-		size_t bytesSize = msg.size() * 2;
+		size_t bytesSize = msg.size() * 2 + 10;
 		char* bytes = tempPool.pstralloc(bytesSize);
 
 		LogString::const_iterator msgIter(msg.begin());
